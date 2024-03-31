da13c3a197f8826c99d7baf5870ab68b4ba5724d
supernovahs <supernovahs@proton.me>
2024-03-31 19:00:15 +0530

    signer v2 clean up

--- crates/signerv2/src/lib.rs
+++ crates/signerv2/src/lib.rs
@@ -1,12 +1,13 @@
+use crate::error::SignerV2Error;
 use eth_keystore::decrypt_key;
 use ethers::{core::types::Signature, signers::Wallet};
-use ethers_core::types::{transaction::eip2718::TypedTransaction, Address};
+use ethers_core::types::transaction::eip2718::TypedTransaction;
 use ethers_signers::Signer;
-use std::fs;
-use std::str::FromStr;
+
+pub mod error;
 
 pub trait SignerV2 {
-    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, String>;
+    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, SignerV2Error>;
 }
 
 #[derive(Clone, Debug)]
@@ -27,24 +28,44 @@ impl PrivateKeySigner {
 }
 
 impl SignerV2 for PrivateKeySigner {
-    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, String> {
-        let wallet = Wallet::from_bytes(&self.private_key).unwrap();
+    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, SignerV2Error> {
+        let wallet_result = Wallet::from_bytes(&self.private_key);
 
-        let signer_tx = wallet.sign_transaction(tx).await.unwrap();
+        match wallet_result {
+            Ok(wallet) => {
+                let signer_tx_result = wallet.sign_transaction(tx).await;
 
-        Ok(signer_tx)
+                match signer_tx_result {
+                    Ok(signer_tx) => {
+                        return Ok(signer_tx);
+                    }
+                    Err(_) => return Err(SignerV2Error::SignTransaction),
+                }
+            }
+            Err(_) => return Err(SignerV2Error::BuildWallet),
+        }
     }
 }
 
 impl SignerV2 for KeyStoreSigner {
-    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, String> {
+    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, SignerV2Error> {
         let private_key = decrypt_key(&self.path, &self.password).unwrap();
 
-        let wallet = Wallet::from_bytes(&private_key).unwrap();
+        let wallet_result = Wallet::from_bytes(&private_key);
 
-        let signer_tx = wallet.sign_transaction(tx).await.unwrap();
+        match wallet_result {
+            Ok(wallet) => {
+                let signer_tx_result = wallet.sign_transaction(tx).await;
 
-        Ok(signer_tx)
+                match signer_tx_result {
+                    Ok(signer_tx) => {
+                        return Ok(signer_tx);
+                    }
+                    Err(_) => return Err(SignerV2Error::SignTransaction),
+                }
+            }
+            Err(_) => return Err(SignerV2Error::BuildWallet),
+        }
     }
 }
 
@@ -52,8 +73,6 @@ pub struct Config {
     private_key: Vec<u8>,
     keystore_path: String,
     password: String,
-    client: String,
-    address: String,
 }
 
 impl Config {
@@ -64,13 +83,6 @@ impl Config {
     fn is_local_key_store_signer(&self) -> bool {
         !self.keystore_path.is_empty()
     }
-
-    fn is_remote_signer(&self) -> bool {
-        if self.client.is_empty() && self.address.is_empty() {
-            return true;
-        }
-        false
-    }
 }
 
 pub struct SignerFromCOonfig {
@@ -78,23 +90,55 @@ pub struct SignerFromCOonfig {
 }
 
 impl SignerV2 for SignerFromCOonfig {
-    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, String> {
+    async fn sign_transaction(&self, tx: &TypedTransaction) -> Result<Signature, SignerV2Error> {
         if self.c.is_local_key_store_signer() {
-            let private_key = decrypt_key(&self.c.keystore_path, &self.c.password).unwrap();
+            let private_key_result = decrypt_key(&self.c.keystore_path, &self.c.password);
 
-            let wallet = Wallet::from_bytes(&private_key).unwrap();
+            match private_key_result {
+                Ok(private_key) => {
+                    let wallet_result = Wallet::from_bytes(&private_key);
 
-            let signer_tx = wallet.sign_transaction(tx).await.unwrap();
+                    match wallet_result {
+                        Ok(wallet) => {
+                            let signer_tx_result = wallet.sign_transaction(tx).await;
 
-            Ok(signer_tx)
+                            match signer_tx_result {
+                                Ok(signer_tx) => {
+                                    return Ok(signer_tx);
+                                }
+                                Err(_) => return Err(SignerV2Error::SignTransaction),
+                            }
+                        }
+                        Err(_) => return Err(SignerV2Error::BuildWallet),
+                    }
+                }
+                Err(_) => return Err(SignerV2Error::Decryptkey),
+            }
         } else if self.c.is_private_key_signer() {
-            let wallet = Wallet::from_bytes(&self.c.private_key).unwrap();
+            let private_key_result = decrypt_key(&self.c.keystore_path, &self.c.password);
 
-            let signer_tx = wallet.sign_transaction(tx).await.unwrap();
+            match private_key_result {
+                Ok(private_key) => {
+                    let wallet_result = Wallet::from_bytes(&private_key);
 
-            Ok(signer_tx)
+                    match wallet_result {
+                        Ok(wallet) => {
+                            let signer_tx_result = wallet.sign_transaction(tx).await;
+
+                            match signer_tx_result {
+                                Ok(signer_tx) => {
+                                    return Ok(signer_tx);
+                                }
+                                Err(_) => return Err(SignerV2Error::SignTransaction),
+                            }
+                        }
+                        Err(_) => return Err(SignerV2Error::BuildWallet),
+                    }
+                }
+                Err(_) => return Err(SignerV2Error::BuildWallet),
+            }
         } else {
-            return Err("fljsdlf".to_string());
+            return Err(SignerV2Error::ConfigNotFound);
         }
     }
 }
