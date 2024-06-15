8a39f32cf8df0ceedd8db3ad5fd9821ecbbb065c
supernovahs <supernovahs@proton.me>
2024-06-15 18:22:46 +0530

    add mainnet contract address constants

---
+++ testing/testing-utils/src/mainnet_constants.rs
@@ -0,0 +1,40 @@
+use alloy_primitives::{address, Address};
+
+const DELEGATION_MANAGER_ADDRESS: Address = address!("39053D51B77DC0d36036Fc1fCc8Cb819df8Ef37A");
+const STRATEGY_MANAGER_ADDRESS: Address = address!("858646372CC42E1A627fcE94aa7A7033e7CF075A");
+const EIGENPOD_MANAGER_ADDRESS: Address = address!("91E677b07F7AF907ec9a428aafA9fc14a0d3A338");
+const AVS_DIRECTORY_ADDRESS: Address = address!("135dda560e946695d6f155dacafc6f1f25c1f5af");
+const SLASHER_ADDRESS: Address = address!("D92145c07f8Ed1D392c1B88017934E301CC1c3Cd");
+#[allow(non_upper_case_globals)]
+const StrategyBase_cbETH: Address = address!("54945180dB7943c0ed0FEE7EdaB2Bd24620256bc");
+
+#[allow(non_upper_case_globals)]
+const StrategyBase_stETH: Address = address!("93c4b944D05dfe6df7645A86cd2206016c51564D");
+#[allow(non_upper_case_globals)]
+const StrategyBase_rETH: Address = address!("1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2");
+#[allow(non_upper_case_globals)]
+const StrategyBase_ETHx: Address = address!("9d7eD45EE2E8FC5482fa2428f15C971e6369011d");
+#[allow(non_upper_case_globals)]
+const StrategyBase_ankrETH: Address = address!("13760F50a9d7377e4F20CB8CF9e4c26586c658ff");
+
+#[allow(non_upper_case_globals)]
+const StrategyBase_OETH: Address = address!("a4C637e0F704745D182e4D38cAb7E7485321d059");
+#[allow(non_upper_case_globals)]
+const StrategyBase_osETH: Address = address!("57ba429517c3473B6d34CA9aCd56c0e735b94c02");
+#[allow(non_upper_case_globals)]
+const StrategyBase_swETH: Address = address!("0Fe4F44beE93503346A3Ac9EE5A26b130a5796d6");
+#[allow(non_upper_case_globals)]
+const StrategyBase_wBETH: Address = address!("7CA911E83dabf90C90dD3De5411a10F1A6112184");
+#[allow(non_upper_case_globals)]
+const StrategyBase_sfrxETH: Address = address!("8CA7A5d6f3acd3A7A8bC468a8CD0FB14B6BD28b6");
+#[allow(non_upper_case_globals)]
+const StrategyBase_lsETH: Address = address!("Ae60d8180437b5C34bB956822ac2710972584473");
+#[allow(non_upper_case_globals)]
+const StrategyBase_mETH: Address = address!("298aFB19A105D59E74658C4C334Ff360BadE6dd2");
+const BEACON_CHAIN: Address = address!("aCB55C530Acdb2849e6d4f36992Cd8c9D50ED8F7");
+const EIGEN_POD_BEACON: Address = address!("5a2a4F2F3C18f09179B6703e63D9eDD165909073");
+const DELAYED_WITHDRAWAL_ROUTER: Address = address!("7Fe7E9CC0F274d2435AD5d56D5fa73E47F6A23D8");
+const EIGEN_LAYER_BEACON_ORACLE: Address = address!("343907185b71aDF0eBa9567538314396aa985442");
+const EIGEN_TOKEN: Address = address!("ec53bf9167f50cdeb3ae105f56099aaab9061f83");
+const BEIGEN: Address = address!("83E9115d334D248Ce39a6f36144aEaB5b3456e75");
+const SIGNED_DISTRIBUTOR: Address = address!("035bdAeaB85E47710C27EdA7FD754bA80aD4ad02");
