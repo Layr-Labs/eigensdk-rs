54e447316bf8d49430211d064e45c533b24d72e2
supernovahs <supernovahs@proton.me>
2024-06-17 15:35:28 +0530

    wip

--- examples/avsregistry-write/examples/register_operator_in_quorum_with_avs_registry_coordinator.rs
+++ examples/avsregistry-write/examples/register_operator_in_quorum_with_avs_registry_coordinator.rs
@@ -1,12 +1,16 @@
 //! register operator in quorum with avs registry coordinator
 use eigen_client_avsregistry::writer::AvsRegistryChainWriter;
-
+use eigen_testing_utils::m2_holesky_constants::{
+    BLS_APK_REGISTRY, OPERATOR_STATE_RETRIEVER, REGISTRY_COORDINATOR, STAKE_REGISTRY,SERVICE_MANAGER_ADDRESS,SLASHER_ADDRESS,DELEGATION_MANAGER_ADDRESS,AVS_DIRECTORY_ADDRESS
+};
+use eigen_client_elcontracts::reader::ELChainReader;
 
 #[tokio::main]
 async fn main() {
 
   let holesky_provider = "https://holesky.drpc.org";
-  let avs_registry_writer = AvsRegistryChainWriter::new();
+  let el_chain_reader:ELChainReader = ELChainReader::new(SLASHER_ADDRESS,DELEGATION_MANAGER_ADDRESS,AVS_DIRECTORY_ADDRESS,holesky_provider.to_string());
+  let avs_registry_writer = AvsRegistryChainWriter::new(SERVICE_MANAGER_ADDRESS,REGISTRY_COORDINATOR,OPERATOR_STATE_RETRIEVER,STAKE_REGISTRY,BLS_APK_REGISTRY,el_chain_reader,holesky_provider.to_string());
 
     
 
