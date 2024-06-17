a7468cb7f3e68c9a80a10f26ef6d3522f1429548
supernovahs <supernovahs@proton.me>
2024-06-17 15:21:14 +0530

    wip

--- examples/avsregistry-write/examples/register_operator_in_quorum_with_avs_registry_coordinator.rs
+++ examples/avsregistry-write/examples/register_operator_in_quorum_with_avs_registry_coordinator.rs
@@ -1,2 +1,14 @@
+//! register operator in quorum with avs registry coordinator
+use eigen_client_avsregistry::writer::AvsRegistryChainWriter;
+
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+
+  let holesky_provider = "https://holesky.drpc.org";
+  let avs_registry_writer = AvsRegistryChainWriter::new();
+
+    
+
+
+}
