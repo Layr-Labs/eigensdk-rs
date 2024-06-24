0b7f2a5aebb43b0f682cb5b8565f5db3d5fa95cf
supernovahs <supernovahs@proton.me>
2024-06-24 17:52:58 +0530

    test_calculate_operator_avs_registration_digest_hash

--- crates/chainio/clients/elcontracts/src/reader.rs
+++ crates/chainio/clients/elcontracts/src/reader.rs
@@ -226,10 +226,11 @@ mod tests {
 
     use super::*;
     use alloy_eips::eip1898::BlockNumberOrTag::Number;
-    use alloy_primitives::address;
+    use alloy_primitives::{address, keccak256};
     use alloy_provider::Provider;
     use eigen_testing_utils::anvil_constants::{self, ANVIL_RPC_URL};
     use eigen_utils::binding::mockAvsServiceManager;
+    use AVSDirectory::calculateOperatorAVSRegistrationDigestHashReturn;
     use DelegationManager::calculateDelegationApprovalDigestHashReturn;
     async fn build_el_chain_reader() -> ELChainReader {
         let delegation_manager_address = anvil_constants::get_delegation_manager_address().await;
@@ -321,6 +322,46 @@ mod tests {
             let calculateDelegationApprovalDigestHashReturn { _0: digest_hash } = hash;
 
             assert_eq!(digest_hash, calculate_digest_hash);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_calculate_operator_avs_registration_digest_hash() {
+        let el_chain_reader = build_el_chain_reader().await;
+        let operator: Address = address!("5eb15C0992734B5e77c888D713b4FC67b3D679A2");
+        let avs = Address::from_slice(&keccak256("avs ")[0..20]);
+        let salt: FixedBytes<32> = FixedBytes::from([
+            0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
+            0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
+            0x02, 0x02, 0x02, 0x02,
+        ]);
+        let current_block_number = ANVIL_RPC_URL.clone().get_block_number().await.unwrap();
+        let block_info = ANVIL_RPC_URL
+            .clone()
+            .get_block_by_number(Number(current_block_number), true)
+            .await
+            .unwrap();
+        if let Some(block) = block_info {
+            let timestamp = block.header.timestamp;
+            let expiry = U256::from::<u64>(timestamp + 100);
+            let operator_hash = el_chain_reader
+                .calculate_operator_avs_registration_digest_hash(operator, avs, salt, expiry)
+                .await
+                .unwrap();
+
+            // Using bindings directly to compare with sdk's output
+            let avs_registry_contract =
+                AVSDirectory::new(el_chain_reader.avs_directory, ANVIL_RPC_URL.clone());
+            let operator_hash_from_bindings = avs_registry_contract
+                .calculateOperatorAVSRegistrationDigestHash(operator, avs, salt, expiry)
+                .call()
+                .await
+                .unwrap();
+
+            let calculateOperatorAVSRegistrationDigestHashReturn { _0: hash } =
+                operator_hash_from_bindings;
+
+            assert_eq!(hash, operator_hash);
         }
     }
 }
