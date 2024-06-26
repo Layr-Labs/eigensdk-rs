04f75bcceffca8389161a7f287635ccbb314dc38
supernovahs <supernovahs@proton.me>
2024-06-26 17:50:55 +0530

    add etherscan links for mainnet addresses

--- testing/testing-utils/src/mainnet_constants.rs
+++ testing/testing-utils/src/mainnet_constants.rs
@@ -1,43 +1,64 @@
 use alloy_primitives::{address, Address};
-
+/// https://etherscan.io/address/0x39053D51B77DC0d36036Fc1fCc8Cb819df8Ef37A
 pub const DELEGATION_MANAGER_ADDRESS: Address =
     address!("39053D51B77DC0d36036Fc1fCc8Cb819df8Ef37A");
+/// https://etherscan.io/address/0x858646372CC42E1A627fcE94aa7A7033e7CF075A
 pub const STRATEGY_MANAGER_ADDRESS: Address = address!("858646372CC42E1A627fcE94aa7A7033e7CF075A");
+/// https://etherscan.io/address/0x91E677b07F7AF907ec9a428aafA9fc14a0d3A338
 pub const EIGENPOD_MANAGER_ADDRESS: Address = address!("91E677b07F7AF907ec9a428aafA9fc14a0d3A338");
+/// https://etherscan.io/address/0x135dda560e946695d6f155dacafc6f1f25c1f5af
 pub const AVS_DIRECTORY_ADDRESS: Address = address!("135dda560e946695d6f155dacafc6f1f25c1f5af");
+/// https://etherscan.io/address/0xD92145c07f8Ed1D392c1B88017934E301CC1c3Cd
 pub const SLASHER_ADDRESS: Address = address!("D92145c07f8Ed1D392c1B88017934E301CC1c3Cd");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x54945180dB7943c0ed0FEE7EdaB2Bd24620256bc
 pub const StrategyBase_cbETH: Address = address!("54945180dB7943c0ed0FEE7EdaB2Bd24620256bc");
-
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x93c4b944D05dfe6df7645A86cd2206016c51564D
 pub const StrategyBase_stETH: Address = address!("93c4b944D05dfe6df7645A86cd2206016c51564D");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2
 pub const StrategyBase_rETH: Address = address!("1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x9d7eD45EE2E8FC5482fa2428f15C971e6369011d
 pub const StrategyBase_ETHx: Address = address!("9d7eD45EE2E8FC5482fa2428f15C971e6369011d");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/13760F50a9d7377e4F20CB8CF9e4c26586c658ff
 pub const StrategyBase_ankrETH: Address = address!("13760F50a9d7377e4F20CB8CF9e4c26586c658ff");
-
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0xa4C637e0F704745D182e4D38cAb7E7485321d059
 pub const StrategyBase_OETH: Address = address!("a4C637e0F704745D182e4D38cAb7E7485321d059");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x57ba429517c3473B6d34CA9aCd56c0e735b94c02
 pub const StrategyBase_osETH: Address = address!("57ba429517c3473B6d34CA9aCd56c0e735b94c02");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x0Fe4F44beE93503346A3Ac9EE5A26b130a5796d6
 pub const StrategyBase_swETH: Address = address!("0Fe4F44beE93503346A3Ac9EE5A26b130a5796d6");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x7CA911E83dabf90C90dD3De5411a10F1A6112184
 pub const StrategyBase_wBETH: Address = address!("7CA911E83dabf90C90dD3De5411a10F1A6112184");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x8CA7A5d6f3acd3A7A8bC468a8CD0FB14B6BD28b6
 pub const StrategyBase_sfrxETH: Address = address!("8CA7A5d6f3acd3A7A8bC468a8CD0FB14B6BD28b6");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0xAe60d8180437b5C34bB956822ac2710972584473
 pub const StrategyBase_lsETH: Address = address!("Ae60d8180437b5C34bB956822ac2710972584473");
 #[allow(non_upper_case_globals)]
+/// https://etherscan.io/address/0x298aFB19A105D59E74658C4C334Ff360BadE6dd2
 pub const StrategyBase_mETH: Address = address!("298aFB19A105D59E74658C4C334Ff360BadE6dd2");
+/// https://etherscan.io/address/0xbeaC0eeEeeeeEEeEeEEEEeeEEeEeeeEeeEEBEaC0
 pub const BEACON_CHAIN_ETH: Address = address!("beaC0eeEeeeeEEeEeEEEEeeEEeEeeeEeeEEBEaC0");
+/// https://etherscan.io/address/0x7261C2bd75a7ACE1762f6d7FAe8F63215581832D
 pub const EIGEN_POD_BEACON: Address = address!("7261C2bd75a7ACE1762f6d7FAe8F63215581832D");
+/// https://etherscan.io/address/0x7Fe7E9CC0F274d2435AD5d56D5fa73E47F6A23D8
 pub const DELAYED_WITHDRAWAL_ROUTER: Address = address!("7Fe7E9CC0F274d2435AD5d56D5fa73E47F6A23D8");
+/// https://etherscan.io/address/0x343907185b71aDF0eBa9567538314396aa985442
 pub const EIGEN_LAYER_BEACON_ORACLE: Address = address!("343907185b71aDF0eBa9567538314396aa985442");
+/// https://etherscan.io/address/0xec53bf9167f50cdeb3ae105f56099aaab9061f83
 pub const EIGEN_TOKEN: Address = address!("ec53bf9167f50cdeb3ae105f56099aaab9061f83");
+/// https://etherscan.io/address/0x83E9115d334D248Ce39a6f36144aEaB5b3456e75
 pub const BEIGEN: Address = address!("83E9115d334D248Ce39a6f36144aEaB5b3456e75");
+/// https://etherscan.io/address/0x035bdAeaB85E47710C27EdA7FD754bA80aD4ad02
 pub const SIGNED_DISTRIBUTOR: Address = address!("035bdAeaB85E47710C27EdA7FD754bA80aD4ad02");
-
+/// https://etherscan.io/address/0xaCB55C530Acdb2849e6d4f36992Cd8c9D50ED8F7
 pub const EIGEN_STRATEGY: Address = address!("aCB55C530Acdb2849e6d4f36992Cd8c9D50ED8F7");
