b4c2804bc1c32dac66d90e00d32cb934c4e38576
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-07-25 16:54:51 -0300

    progress in SimpleTxManager

--- crates/chainio/txmanager/src/simple_tx_manager.rs
+++ crates/chainio/txmanager/src/simple_tx_manager.rs
@@ -1,12 +1,7 @@
-/*
-type SimpleTxManager struct {
-    wallet             wallet.Wallet
-    client             eth.Client
-    log                logging.Logger
-    sender             common.Address
-    gasLimitMultiplier float64
-}
-*/
+use alloy_network::{Ethereum, EthereumWallet};
+use alloy_primitives::{address, fixed_bytes, Address, Bytes, FixedBytes, B256, I256, U256};
+//use alloy_provider::Provider;
+//use alloy_rpc_types::Filter;
 
 // TODO!!!
 
@@ -15,10 +10,34 @@ type SimpleTxManager struct {
 //use std::path::Path;
 //
 //
-//pub struct SimpleTxManager {
-//    wallet: wallet::Wallet,
-//    client: eth::Client,
-//    log: logging::Logger,
-//    sender: common::Address,
-//    gas_limit_multiplier: f64,
-//}
+pub struct SimpleTxManager {
+    wallet: EthereumWallet,
+    //    client: eth::Client,
+    //    log: logging::Logger,
+    sender: Address,
+    gas_limit_multiplier: f64,
+}
+
+impl SimpleTxManager {
+    /*
+    pub fn new(
+        wallet: wallet::Wallet,
+        client: eth::Client,
+        log: logging::Logger,
+        sender: common::Address,
+        gas_limit_multiplier: f64,
+    ) -> SimpleTxManager {
+        SimpleTxManager {
+            wallet,
+            client,
+            log,
+            sender,
+            gas_limit_multiplier,
+        }
+    }
+    */
+
+    pub fn with_gas_limit_multiplier(&mut self, multiplier: f64) {
+        self.gas_limit_multiplier = multiplier;
+    }
+}
