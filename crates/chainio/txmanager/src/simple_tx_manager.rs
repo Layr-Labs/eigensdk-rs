928deab6084aeff6bcfcbcd0f50612c1753eb744
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-07-26 19:01:47 -0300

    simple tx manager progress

--- crates/chainio/txmanager/src/simple_tx_manager.rs
+++ crates/chainio/txmanager/src/simple_tx_manager.rs
@@ -1,3 +1,11 @@
+//use alloy::{
+//    network::TransactionBuilder,
+//    node_bindings::Anvil,
+//    primitives::U256,
+//    providers::{Provider, ProviderBuilder},
+//    rpc::types::TransactionRequest,
+//};
+
 use alloy_network::{Ethereum, EthereumWallet};
 use alloy_primitives::{address, fixed_bytes, Address, Bytes, FixedBytes, B256, I256, U256};
 //use alloy_provider::Provider;
@@ -39,5 +47,59 @@ impl SimpleTxManager {
 
     pub fn with_gas_limit_multiplier(&mut self, multiplier: f64) {
         self.gas_limit_multiplier = multiplier;
+    }
+
+    /*
+        // Spin up a local Anvil node.
+        // Ensure `anvil` is available in $PATH.
+        let anvil = Anvil::new().try_spawn()?;
+
+        // Create a provider.
+        let rpc_url = anvil.endpoint().parse()?;
+        let provider = ProviderBuilder::new().on_http(rpc_url);
+
+        // Create two users, Alice and Bob.
+        let alice = anvil.addresses()[0];
+        let bob = anvil.addresses()[1];
+
+        // Build a transaction to send 100 wei from Alice to Bob.
+        // The `from` field is automatically filled to the first signer's address (Alice).
+        let tx = TransactionRequest::default()
+            .with_to(bob)
+            .with_nonce(0)
+            .with_chain_id(anvil.chain_id())
+            .with_value(U256::from(100))
+            .with_gas_limit(21_000)
+            .with_max_priority_fee_per_gas(1_000_000_000)
+            .with_max_fee_per_gas(20_000_000_000);
+
+        // Send the transaction and wait for the broadcast.
+        let pending_tx = provider.send_transaction(tx).await?;
+
+        println!("Pending transaction... {}", pending_tx.tx_hash());
+
+        // Wait for the transaction to be included and get the receipt.
+        let receipt = pending_tx.get_receipt().await?;
+
+        println!(
+            "Transaction included in block {}",
+            receipt.block_number.expect("Failed to get block number")
+        );
+
+        assert_eq!(receipt.from, alice);
+        assert_eq!(receipt.to, Some(bob));
+
+        Ok(())
+    */
+
+    pub fn send() {
+        let tx = TransactionRequest::default()
+            .with_to(bob)
+            .with_nonce(0)
+            .with_chain_id(anvil.chain_id())
+            .with_value(U256::from(100))
+            .with_gas_limit(21_000)
+            .with_max_priority_fee_per_gas(1_000_000_000)
+            .with_max_fee_per_gas(20_000_000_000);
     }
 }
