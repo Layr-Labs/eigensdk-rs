eaa9d49950267d88f2aef9a12055ea46d021d46f
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-08-05 17:58:58 -0300

    first steps of pub trait BackendClient

--- crates/chainio/clients/eth/src/client.rs
+++ crates/chainio/clients/eth/src/client.rs
@@ -1,2 +1,23 @@
 #[derive(Debug)]
 pub struct Client {}
+
+use async_trait::async_trait;
+use ethers::providers::Middleware;
+use ethers::types::{Block, BlockNumber, U64};
+
+pub enum BackendType {
+    HttpBAckend,
+    WasmBackend,
+}
+
+#[async_trait::async_trait]
+pub trait BackendClient {
+    type Error;
+
+    async fn block_number(&self, backend_type: &BackendType) -> Result<U64, Self::Error>;
+    async fn block_by_number(
+        &self,
+        backend_type: &BackendType,
+        number: Option<BlockNumber>,
+    ) -> Result<Option<Block<ethers::types::TxHash>>, Self::Error>;
+}
