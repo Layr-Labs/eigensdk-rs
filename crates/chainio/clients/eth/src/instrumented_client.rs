d45309b767fee33728c92473c8e9e7ffaec903bc
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-08-06 17:36:57 -0300

    instrument_function

--- crates/chainio/clients/eth/src/instrumented_client.rs
+++ crates/chainio/clients/eth/src/instrumented_client.rs
@@ -1,13 +1,16 @@
 //use eigen_metrics_collectors_rpc_calls::RpcCalls as RpcCallsCollector;
+use alloy_json_rpc::{RpcParam, RpcReturn};
 use alloy_provider::{Provider, ProviderBuilder, RootProvider};
-use alloy_transport_http::{Client, Http};
+use alloy_transport::TransportResult;
+use alloy_transport_http::{reqwest::Method, Client, Http};
+use eigen_metrics_collectors_rpc_calls::RpcCalls;
+use std::time::Instant;
 use thiserror::Error;
 use url::Url;
 
-use crate::client;
-
 pub struct InstrumentedClient {
     client: RootProvider<Http<Client>>,
+    rpc_collector: RpcCalls,
     net_version: u64,
 }
 
@@ -29,8 +32,10 @@ impl InstrumentedClient {
             .get_net_version()
             .await
             .map_err(|_| InstrumentedClientError::ErrorGettingVersion)?;
+        let rpc_collector = RpcCalls::new();
         Ok(InstrumentedClient {
             client,
+            rpc_collector,
             net_version,
         })
     }
@@ -43,11 +48,64 @@ impl InstrumentedClient {
             .await
             .map_err(|_| InstrumentedClientError::ErrorGettingVersion)?;
 
+        let rpc_collector = RpcCalls::new();
         Ok(InstrumentedClient {
             client: client.clone(),
+            rpc_collector,
             net_version,
         })
     }
+
+    async fn instrument_function<'async_trait, P, R>(
+        &self,
+        rpc_method_name: &str,
+        params: P,
+    ) -> TransportResult<R>
+    where
+        P: RpcParam + 'async_trait,
+        R: RpcReturn + 'async_trait,
+    {
+        let start = Instant::now();
+        let method_string = String::from(rpc_method_name);
+        let result = self.client.raw_request(method_string.into(), params).await;
+        let rpc_request_duration = start.elapsed();
+
+        // we only observe the duration of successful calls (even though this is not well defined in the spec)
+        self.rpc_collector.set_rpc_request_duration_seconds(
+            rpc_method_name,
+            self.net_version.to_string().as_str(),
+            rpc_request_duration.as_secs_f64(),
+        );
+
+        result
+    }
+
+    /*
+    // Generic function used to instrument all the eth calls that we make below
+    func instrumentFunction[T any](
+        rpcCall func() (T, error),
+        rpcMethodName string,
+        iec *InstrumentedClient,
+    ) (value T, err error) {
+        start := time.Now()
+        result, err := rpcCall()
+        // we count both successful and erroring calls (even though this is not well defined in the spec)
+        iec.rpcCallsCollector.AddRPCRequestTotal(rpcMethodName, iec.clientAndVersion)
+        if err != nil {
+            return value, err
+        }
+        rpcRequestDuration := time.Since(start)
+        // we only observe the duration of successful calls (even though this is not well defined in the spec)
+        iec.rpcCallsCollector.ObserveRPCRequestDurationSeconds(
+            float64(rpcRequestDuration),
+            rpcMethodName,
+            iec.clientAndVersion,
+        )
+        return result, nil
+    }
+
+
+         */
 }
 
 // https://docs.rs/alloy-provider/0.1.4/alloy_provider/trait.Provider.html#method.raw_request
