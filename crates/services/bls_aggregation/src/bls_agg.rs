21c49df9067d3790a6d433b57e4f6c6a97d72983
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-08-19 15:19:56 -0300

    add test

--- crates/services/bls_aggregation/src/bls_agg.rs
+++ crates/services/bls_aggregation/src/bls_agg.rs
@@ -429,7 +429,6 @@ mod tests {
     use std::collections::HashMap;
     use std::time::Duration;
     use std::vec;
-    use tokio::time::timeout;
 
     use super::{BlsAggregationServiceError, BlsAggregationServiceResponse, BlsAggregatorService};
 
@@ -1096,6 +1095,71 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn test_1_quorum_2_operator_1_signatures_60_threshold() {
+        let test_operator_1 = TestOperator {
+            operator_id: U256::from(1).into(),
+            stake_per_quorum: HashMap::from([(0u8, U256::from(100)), (1u8, U256::from(200))]),
+            bls_keypair: new_bls_key_pair_panics(
+                "13710126902690889134622698668747132666439281256983827313388062967626731803599"
+                    .into(),
+            ),
+        };
+        let test_operator_2 = TestOperator {
+            operator_id: U256::from(2).into(),
+            stake_per_quorum: HashMap::from([(0u8, U256::from(100)), (1u8, U256::from(200))]),
+            bls_keypair: new_bls_key_pair_panics(
+                "14610126902690889134622698668747132666439281256983827313388062967626731803500"
+                    .into(),
+            ),
+        };
+        let test_operators = vec![test_operator_1.clone(), test_operator_2.clone()];
+        let block_number = 1;
+        let task_index = 0;
+        let quorum_numbers: Vec<QuorumNum> = vec![0];
+        let quorum_threshold_percentages: QuorumThresholdPercentages = vec![60u8];
+        let time_to_expiry = Duration::from_secs(1);
+        let task_response = 123; // Initialize with appropriate data
+        let task_response_digest = hash(task_response);
+        let bls_sig_op_1 = test_operator_1
+            .bls_keypair
+            .sign_message(task_response_digest.as_ref());
+
+        let fake_avs_registry_service = FakeAvsRegistryService::new(block_number, test_operators);
+        let bls_agg_service = BlsAggregatorService::new(fake_avs_registry_service);
+
+        bls_agg_service
+            .initialize_new_task::<FakeAvsRegistryService>(
+                task_index,
+                block_number as u32,
+                quorum_numbers,
+                quorum_threshold_percentages,
+                time_to_expiry,
+            )
+            .await;
+        bls_agg_service
+            .process_new_signature(
+                task_index,
+                task_response_digest,
+                bls_sig_op_1,
+                test_operator_1.operator_id,
+            )
+            .await
+            .unwrap();
+
+        let response = bls_agg_service
+            .aggregated_response_receiver
+            .lock()
+            .await
+            .recv()
+            .await;
+
+        assert_eq!(
+            Err(BlsAggregationServiceError::TaskExpired),
+            response.unwrap()
+        );
+    }
+
+    #[tokio::test]
     async fn test_2_quorums_2_operators_which_just_take_1_quorum_2_correct_signatures() {
         let test_operator_1 = TestOperator {
             operator_id: U256::from(1).into(),
@@ -1719,4 +1783,7 @@ mod tests {
 
         assert_eq!(Err(BlsAggregationServiceError::TaskExpired), result);
     }
+
+    #[tokio::test]
+    async fn test() {}
 }
