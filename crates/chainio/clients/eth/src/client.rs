f074e756a61a705d349b413b610e3c194174d2c6
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-08-19 16:03:44 -0300

    docs in pub trait BackendClient

--- crates/chainio/clients/eth/src/client.rs
+++ crates/chainio/clients/eth/src/client.rs
@@ -12,7 +12,27 @@ pub enum BackendType {
 pub trait BackendClient {
     type Error;
 
+    /// Get the latest block number.
+    ///
+    /// # Arguments
+    ///
+    /// * `backend_type` - The type of the backend: HttpBackend or WasmBackend.
+    ///
+    /// # Returns
+    ///
+    /// The latest block number.
     async fn block_number(&self, backend_type: &BackendType) -> Result<u64, Self::Error>;
+
+    /// Get the block hash given its block number.
+    ///
+    /// # Arguments
+    ///
+    /// * `backend_type` - The type of the backend: HttpBackend or WasmBackend.
+    /// * `number` - The block number.
+    ///
+    /// # Returns
+    ///
+    /// The block hash of the block having that number.
     async fn block_by_number(
         &self,
         backend_type: &BackendType,
