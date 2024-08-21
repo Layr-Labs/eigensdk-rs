fa5456ca468076bd7f7a0fe3e338b18b8b1e25a3
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-08-21 11:37:51 -0300

    update integration test to start start operators info service

--- crates/services/bls_aggregation/src/bls_agg_test.rs
+++ crates/services/bls_aggregation/src/bls_agg_test.rs
@@ -1,10 +1,7 @@
 #[cfg(test)]
 pub mod integration_test {
-    use std::time::Duration;
-
     use crate::bls_agg::BlsAggregatorService;
-    use alloy_primitives::{Bytes, B256};
-    use alloy_primitives::{FixedBytes, U256};
+    use alloy_primitives::{Bytes, FixedBytes, B256, U256};
     use alloy_provider::Provider;
     use eigen_client_avsregistry::{
         reader::AvsRegistryChainReader, writer::AvsRegistryChainWriter,
@@ -15,13 +12,13 @@ pub mod integration_test {
     use eigen_services_operatorsinfo::operatorsinfo_inmemory::OperatorInfoServiceInMemory;
     use eigen_testing_utils::anvil_constants::{
         get_operator_state_retriever_address, get_registry_coordinator_address,
-        get_service_manager_address, ANVIL_RPC_URL,
+        get_service_manager_address,
     };
-    use eigen_types::avs::TaskIndex;
-    use eigen_types::operator::{operator_id_from_g1_pub_key, QuorumThresholdPercentages};
-    use eigen_utils::binding::mockAvsServiceManager;
-    use eigen_utils::get_provider;
+    use eigen_types::{avs::TaskIndex, operator::QuorumThresholdPercentages};
+    use eigen_utils::{binding::mockAvsServiceManager, get_provider};
     use sha2::{Digest, Sha256};
+    use std::time::Duration;
+    use tokio::sync::watch;
 
     fn hash(task_response: u64) -> B256 {
         let mut hasher = Sha256::new();
@@ -36,17 +33,12 @@ pub mod integration_test {
         let service_manager_address = get_service_manager_address().await;
         let http_endpoint = "http://localhost:8545".to_string();
         let ws_endpoint = "ws://localhost:8545".to_string();
-
-        let digest_hash: FixedBytes<32> = FixedBytes::from([
-            0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
-            0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
-            0x02, 0x02, 0x02, 0x02,
-        ]);
+        let provider = get_provider(&http_endpoint);
+        let salt: FixedBytes<32> = FixedBytes::from([0x02; 32]);
         let bls_key_pair = BlsKeyPair::new(
             "13710126902690889134622698668747132666439281256983827313388062967626731803599".into(),
         )
         .unwrap();
-        let operator_id = operator_id_from_g1_pub_key(bls_key_pair.public_key());
         let quorum_nums = Bytes::from([0]);
 
         // create avs clients to interact with contracts deployed on anvil
@@ -61,16 +53,12 @@ pub mod integration_test {
         let avs_writer = AvsRegistryChainWriter::build_avs_registry_chain_writer(
             get_test_logger(),
             http_endpoint.clone(),
-            "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80".to_string(), // TODO: use anvil private key
+            "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80".to_string(),
             registry_coordinator_address,
             operator_state_retriever_address,
         )
         .await
         .unwrap();
-        // let avs_registry_subscriber = AvsRegistryChainSubscriber::new(http_endpoint); // is AvsRegistryChainSubscriber fully implemented?
-
-        let service_manager =
-            mockAvsServiceManager::new(service_manager_address, ANVIL_RPC_URL.clone());
 
         // create aggregation service
         let operators_info = OperatorInfoServiceInMemory::new(
@@ -79,29 +67,40 @@ pub mod integration_test {
             ws_endpoint,
         )
         .await;
+
+        let (shutdown_tx, shutdown_rx) = watch::channel(());
+        operators_info
+            .start_service(0, 0, shutdown_rx)
+            .await
+            .unwrap();
+
         let avs_registry_service =
             AvsRegistryServiceChainCaller::new(avs_registry_reader.clone(), operators_info);
 
         let bls_agg_service = BlsAggregatorService::new(avs_registry_service);
 
         // register operator
-        // TODO: check if the operator is registered successfully
         avs_writer
             .register_operator_in_quorum_with_avs_registry_coordinator(
                 bls_key_pair.clone(),
-                digest_hash,
-                U256::from(6300),
+                salt,
+                U256::from_be_slice(&[0xff; 32]), // TODO: check expiry time
                 quorum_nums.clone(),
                 "socket".to_string(),
             )
             .await
             .unwrap();
 
+        // this query advances the block number by 1
+        let operators = avs_registry_reader
+            .get_operators_stake_in_quorums_at_current_block(quorum_nums.clone())
+            .await
+            .unwrap();
+        let operator_id = operators[0][0].operatorId;
+
         // create the task related parameters
-        let provider = get_provider(&http_endpoint);
         let current_block_num = provider.get_block_number().await.unwrap();
         let task_index: TaskIndex = 0;
-        let task_response = 123; // Initialize with appropriate data
         let quorum_threshold_percentages: QuorumThresholdPercentages = vec![100];
         let time_to_expiry = Duration::from_secs(1);
 
@@ -118,13 +117,14 @@ pub mod integration_test {
             .unwrap();
 
         // compute the signature and send it to the aggregation service
-        let task_response_digest = hash(task_response);
-        let bls_sig_op_1 = bls_key_pair.sign_message(task_response_digest.as_ref());
+        let task_response = 123; // Initialize with appropriate data
+        let task_response_digest = hash(task_response); // es igual
+        let bls_signature = bls_key_pair.sign_message(task_response_digest.as_ref()); // es igual
         bls_agg_service
             .process_new_signature(
                 task_index,
                 task_response_digest,
-                bls_sig_op_1.clone(),
+                bls_signature,
                 operator_id.into(),
             )
             .await
@@ -140,6 +140,11 @@ pub mod integration_test {
             .unwrap()
             .unwrap();
 
+        // Send the shutdown signal to the OperatorInfoServiceInMemory
+        let _ = shutdown_tx.send(());
+
         // TODO: check the response signature with `service_manager.checkSignatures()`
+        let service_manager =
+            mockAvsServiceManager::new(service_manager_address, get_provider(&http_endpoint));
     }
 }
