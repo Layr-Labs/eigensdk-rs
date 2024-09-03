a37d3f040a67e75639b83813e827468cb759068a
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-09-03 16:22:08 -0300

    rewardsCoordinator added in DeployMockAvsRegistries.s.sol

--- crates/contracts/script/DeployMockAvs.s.sol
+++ crates/contracts/script/DeployMockAvs.s.sol
@@ -12,9 +12,14 @@ contract DeployMockAvs is DeployMockAvsRegistries {
     function run() public virtual {
         // The ContractsRegistry contract should always be deployed at this address on anvil
         // it's the address of the contract created at nonce 0 by the first anvil account (0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266)
-        ContractsRegistry contractsRegistry = ContractsRegistry(0x5FbDB2315678afecb367f032d93F642f64180aa3);
-        EigenlayerContracts memory eigenlayerContracts = _loadEigenlayerDeployedContracts();
-        MockAvsOpsAddresses memory addressConfig = _loadAvsOpsAddresses("ops_addresses");
+        ContractsRegistry contractsRegistry = ContractsRegistry(
+            0x5FbDB2315678afecb367f032d93F642f64180aa3
+        );
+        EigenlayerContracts
+            memory eigenlayerContracts = _loadEigenlayerDeployedContracts();
+        MockAvsOpsAddresses memory addressConfig = _loadAvsOpsAddresses(
+            "ops_addresses"
+        );
 
         vm.startBroadcast();
 
@@ -24,27 +29,60 @@ contract DeployMockAvs is DeployMockAvsRegistries {
         emptyContract = new EmptyContract();
         mockAvsProxyAdmin = new ProxyAdmin();
         mockAvsServiceManager = MockAvsServiceManager(
-            address(new TransparentUpgradeableProxy(address(emptyContract), address(mockAvsProxyAdmin), ""))
+            address(
+                new TransparentUpgradeableProxy(
+                    address(emptyContract),
+                    address(mockAvsProxyAdmin),
+                    ""
+                )
+            )
         );
-        MockAvsContracts memory mockAvsContracts = _deploymockAvsRegistryContracts(
-            eigenlayerContracts, addressConfig, mockAvsServiceManager, mockAvsServiceManagerImplementation
-        );
+        MockAvsContracts
+            memory mockAvsContracts = _deploymockAvsRegistryContracts(
+                eigenlayerContracts,
+                addressConfig,
+                mockAvsServiceManager,
+                mockAvsServiceManagerImplementation
+            );
         mockAvsServiceManagerImplementation = new MockAvsServiceManager(
-            registryCoordinator, eigenlayerContracts.avsDirectory, eigenlayerContracts.rewardsCoordinator
+            registryCoordinator,
+            eigenlayerContracts.avsDirectory,
+            eigenlayerContracts.rewardsCoordinator
         );
 
         mockAvsProxyAdmin.upgradeAndCall(
-            TransparentUpgradeableProxy(payable(address(mockAvsServiceManager))),
+            TransparentUpgradeableProxy(
+                payable(address(mockAvsServiceManager))
+            ),
             address(mockAvsServiceManagerImplementation),
-            abi.encodeWithSelector(mockAvsServiceManager.initialize.selector, addressConfig.communityMultisig)
+            abi.encodeWithSelector(
+                mockAvsServiceManager.initialize.selector,
+                addressConfig.communityMultisig
+            )
         );
-        require(Ownable(address(mockAvsServiceManager)).owner() != address(0), "Owner uninitialized");
+        require(
+            Ownable(address(mockAvsServiceManager)).owner() != address(0),
+            "Owner uninitialized"
+        );
 
         if (block.chainid == 31337 || block.chainid == 1337) {
-            contractsRegistry.registerContract("ProxyAdmin",address(mockAvsProxyAdmin));
-            contractsRegistry.registerContract("AvsDirectory",address(eigenlayerContracts.avsDirectory));
-            contractsRegistry.registerContract("eigenlayerPauserReg", address(eigenlayerContracts.eigenlayerPauserReg));
-            _writeContractsToRegistry(contractsRegistry, eigenlayerContracts, mockAvsContracts);
+            contractsRegistry.registerContract(
+                "ProxyAdmin",
+                address(mockAvsProxyAdmin)
+            );
+            contractsRegistry.registerContract(
+                "AvsDirectory",
+                address(eigenlayerContracts.avsDirectory)
+            );
+            contractsRegistry.registerContract(
+                "eigenlayerPauserReg",
+                address(eigenlayerContracts.eigenlayerPauserReg)
+            );
+            _writeContractsToRegistry(
+                contractsRegistry,
+                eigenlayerContracts,
+                mockAvsContracts
+            );
         }
         vm.stopBroadcast();
     }
