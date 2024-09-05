7b85591cc1d3f6d427d3b62e074348d90e685ef3
supernovahs <supernovahs@proton.me>
2024-09-05 14:37:28 +0530

    add collect method

--- crates/metrics/collectors/economic/src/lib.rs
+++ crates/metrics/collectors/economic/src/lib.rs
@@ -3,43 +3,201 @@
     issue_tracker_base_url = "https://github.com/Layr-Labs/eigensdk-rs/issues/"
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-use eigen_logging::logger::SharedLogger;
-use metrics::{describe_gauge, gauge, Key, Label};
+use std::{collections::HashMap, str::FromStr};
 
+pub mod error;
+pub mod fake_collector;
+use alloy_primitives::{Address, FixedBytes, U256};
+use eigen_client_avsregistry::reader::AvsRegistryChainReader;
+use eigen_client_elcontracts::reader::ELChainReader;
+use eigen_logging::logger::SharedLogger;
+use eigen_types::operator::OperatorId;
+use error::CollectorMetricError;
+use metrics::{describe_gauge, gauge, Key, Label};
+use num_bigint::BigInt;
 /// RegisteredStakes Metrics with logger
 #[derive(Debug)]
-pub struct RegisteredStakesMetrics {
+pub struct Collector {
     logger: SharedLogger,
+    operator_addr: Address,
+    operator_id: OperatorId,
+    el_reader: ELChainReader,
+    avs_registry_reader: AvsRegistryChainReader,
+    quorum_names: HashMap<u64, String>,
+    avs_name: String,
 }
 
-impl RegisteredStakesMetrics {
+impl Collector {
     /// Operator stakes in AVS registry contract.
     /// Most commonly represents a weighted combination of delegated shares in the DelegationManager EigenLayer contract.
-    pub fn new(logger: SharedLogger) -> Self {
+    pub fn new(
+        logger: SharedLogger,
+        operator_addr: Address,
+        operator_id: OperatorId,
+        el_reader: ELChainReader,
+        avs_registry_reader: AvsRegistryChainReader,
+        quorum_names: HashMap<u64, String>,
+        avs_name: &str,
+    ) -> Self {
         describe_gauge!(
             "eigen_registered_stakes",
             "A gauge with weighted delegation of delegated shares in delegation manager contract"
         );
+        // for now the namespace if "e" instead of eigen as the docs mention to not use the eigen namespace for this metric
+        describe_gauge!(
+            format!("{},_slashing_status", "e"),
+            "Whether the operator has been slashed"
+        );
 
-        Self { logger }
+        Self {
+            logger,
+            operator_addr,
+            el_reader,
+            avs_registry_reader,
+            operator_id,
+            quorum_names,
+            avs_name: avs_name.to_owned(),
+        }
     }
 
-    pub fn set_stake(&self, quorum_number: &str, quorum_name: &str, value: f64) {
-        // Create the metric key with dynamic labels
+    pub fn set_stake(&self, quorum_number: &str, quorum_name: &str, avs_name: &str, value: f64) {
+        // Create the metric key with dynamic
         let key = Key::from_parts(
             "eigen_registered_stakes",
             vec![
                 Label::new("quorum_number", quorum_number.to_string()),
                 Label::new("quorum_name", quorum_name.to_string()),
+                Label::new("avs_name", avs_name.to_string()),
             ],
         );
         gauge!(key.to_string()).set(value);
         self.logger.debug(
             &format!(
-                "set registered stakes , quorum_name: {} , quorum_number: {} , value: {}  ",
-                quorum_name, quorum_number, value
-            ),
+            "set registered stakes , quorum_name: {} , quorum_number: {} , avs_name: {}, value: {}",
+            quorum_name, quorum_number, avs_name, value
+        ),
             "eigen-metrics-collectors-economic.set_stake",
         );
+    }
+
+    pub async fn collect(&mut self) -> Result<(), CollectorMetricError> {
+        let operator_is_frozen = self
+            .el_reader
+            .operator_is_frozen(self.operator_addr)
+            .await?;
+
+        let mut operator_is_frozen_float = 0.0;
+        if operator_is_frozen {
+            operator_is_frozen_float = 1.0;
+        }
+
+        gauge!("e_slashing_status").set(operator_is_frozen_float);
+
+        let operator_id_res = self.init_operator_id().await;
+
+        match operator_id_res {
+            Ok(_) => {
+                let quorum_stake_map = self
+                    .avs_registry_reader
+                    .get_operator_stake_in_quorums_of_operator_at_current_block(self.operator_id)
+                    .await?;
+                for (quorum_num, stake) in quorum_stake_map {
+                    let quorum_name_for_quorum_num = self.quorum_names.get(&(quorum_num as u64));
+                    let key;
+                    if let Some(quorum_name) = quorum_name_for_quorum_num {
+                        key = Key::from_parts(
+                            "eigen_registered_stakes",
+                            vec![
+                                Label::new("quorum_number", quorum_num.to_string()),
+                                Label::new("quorum_name", quorum_name.to_string()),
+                                Label::new("avs_name", self.avs_name.to_string()),
+                            ],
+                        );
+                    } else {
+                        key = Key::from_parts(
+                            "eigen_registered_stakes",
+                            vec![
+                                Label::new("quorum_number", quorum_num.to_string()),
+                                Label::new("quorum_name", "".to_string()),
+                                Label::new("avs_name", self.avs_name.to_string()),
+                            ],
+                        );
+                    }
+                    let u256_intermediate = U256::from_str(&stake.to_string())?;
+                    gauge!(key.to_string()).set(f64::from(u256_intermediate));
+                }
+            }
+            Err(e) => {
+                self.logger.warn(&format!("Failed to fetch and cache operator id. Skipping collection of registeredStake metric. , err {}",e),"eigen-metrics-collectors-economic.collect");
+                return Err(e);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn init_operator_id(&mut self) -> Result<(), CollectorMetricError> {
+        if self.operator_id.eq(&FixedBytes::default()) {
+            let operator_id = self
+                .avs_registry_reader
+                .get_operator_id(self.operator_addr)
+                .await?;
+
+            if operator_id.eq(&FixedBytes::default()) {
+                return Err(CollectorMetricError::OperatorNotRegistered);
+            }
+            self.operator_id = operator_id;
+            return Ok(());
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use alloy_primitives::{Address, FixedBytes};
+    use eigen_client_avsregistry::reader::AvsRegistryChainReader;
+    use eigen_client_elcontracts::reader::ELChainReader;
+    use eigen_logging::get_test_logger;
+    use eigen_testing_utils::anvil_constants;
+
+    use crate::fake_collector::FakeCollector;
+
+    #[tokio::test]
+    async fn test_init_operator_id() {
+        let operator_addr = Address::ZERO;
+        let operator_id = FixedBytes::<32>::default();
+        let http_anvil = "http://localhost:8545";
+        let el_reader = ELChainReader::new(
+            get_test_logger(),
+            Address::ZERO,
+            anvil_constants::get_delegation_manager_address().await,
+            anvil_constants::get_avs_directory_address().await,
+            http_anvil.to_string(),
+        );
+        let avs_registry_reader = AvsRegistryChainReader::new(
+            get_test_logger(),
+            anvil_constants::get_registry_coordinator_address().await,
+            anvil_constants::get_operator_state_retriever_address().await,
+            http_anvil.to_string(),
+        )
+        .await
+        .unwrap();
+
+        let quorums_names = HashMap::new();
+        let avs_name = "eigensdk-rs";
+        let mut collector = FakeCollector::new(
+            get_test_logger(),
+            operator_addr,
+            operator_id,
+            el_reader,
+            avs_registry_reader,
+            quorums_names,
+            avs_name,
+        );
+
+        assert!(collector.init_operator_id().await.is_err());
     }
 }
--- crates/metrics/src/prometheus.rs
+++ crates/metrics/src/prometheus.rs
@@ -17,10 +17,18 @@ pub fn init_registry(socket_addr: SocketAddr) {
 #[cfg(test)]
 
 mod tests {
+    use std::collections::HashMap;
+
     use super::*;
     use crate::eigenmetrics::EigenPerformanceMetrics;
-    use eigen_metrics_collectors_economic::RegisteredStakesMetrics;
+    use alloy_primitives::Address;
+    use alloy_primitives::FixedBytes;
+    use eigen_client_avsregistry::reader::AvsRegistryChainReader;
+    use eigen_client_elcontracts::reader::ELChainReader;
+    use eigen_metrics_collectors_economic::fake_collector::FakeCollector;
     use eigen_metrics_collectors_rpc_calls::RpcCallsMetrics;
+    use eigen_testing_utils::anvil_constants;
+    use num_bigint::BigInt;
     use tokio::time::sleep;
     use tokio::time::Duration;
 
@@ -30,9 +38,62 @@ mod tests {
         let socket: SocketAddr = "127.0.0.1:9091".parse().unwrap();
         init_registry(socket);
 
+        let operator_addr = Address::ZERO;
+        let operator_id = FixedBytes::<32>::default();
+        let http_anvil = "http://localhost:8545";
+        let el_reader = ELChainReader::new(
+            get_test_logger(),
+            Address::ZERO,
+            anvil_constants::get_delegation_manager_address().await,
+            anvil_constants::get_avs_directory_address().await,
+            http_anvil.to_string(),
+        );
+        let avs_registry_reader = AvsRegistryChainReader::new(
+            get_test_logger(),
+            anvil_constants::get_registry_coordinator_address().await,
+            anvil_constants::get_operator_state_retriever_address().await,
+            http_anvil.to_string(),
+        )
+        .await
+        .unwrap();
+
+        let mut quorums_names = HashMap::new();
+        quorums_names.insert(1, "rust".to_string());
+        let avs_name = "eigensdk-rs";
+        let mut collector = FakeCollector::new(
+            get_test_logger(),
+            operator_addr,
+            operator_id,
+            el_reader,
+            avs_registry_reader,
+            quorums_names,
+            avs_name,
+        );
+
+        collector.set_stake("1", "first", avs_name, 2.0);
+        sleep(Duration::from_secs(1)).await;
+        let client = reqwest::Client::new();
+        let mut body = get_metrics_body(&client, "http://127.0.0.1:9091/metrics").await;
+
+        assert!(body.contains("eigen_registered_stakes"));
+        assert!(body.contains("quorum_number___1"));
+        assert!(body.contains("quorum_name___first"));
+        assert!(body.contains("avs_name___eigensdk_rs"));
+        assert!(body.contains("eigen_registered_stakes___quorum_number___1__quorum_name___first__avs_name___eigensdk_rs__ 2"));
+
+        let is_operator_frozen: bool = true;
+        let mut quorum_stake_map = HashMap::new();
+        quorum_stake_map.insert(1, BigInt::from(23));
+        let _ = collector
+            .collect(is_operator_frozen, quorum_stake_map)
+            .await;
+
+        body = get_metrics_body(&client, "http://127.0.0.1:9091/metrics").await;
+
+        assert!(body.contains("eigen_registered_stakes___quorum_number___1__quorum_name___rust__avs_name___eigensdk_rs__ 23"));
+
         // Initialize all the metrics
         let metrics = EigenPerformanceMetrics::new(get_test_logger());
-        let registered_metrics = RegisteredStakesMetrics::new(get_test_logger());
         let rpc_calls = RpcCallsMetrics::new(get_test_logger());
 
         sleep(Duration::from_secs(1)).await;
@@ -54,15 +115,11 @@ mod tests {
         metrics.set_performance_score(80.0);
         rpc_calls.set_rpc_request_duration_seconds("eth_getBlockByNumber", "rethv1.0.3", 100.0);
         rpc_calls.set_rpc_request_total("eth_getBlockByNumber", "rethv1.0.3", 10);
-        registered_metrics.set_stake("4th", "hello Eigen", 8.0);
 
         sleep(Duration::from_secs(1)).await;
 
         body = get_metrics_body(&client, "http://127.0.0.1:9091/metrics").await;
         assert!(body.contains("eigen_performance_score 80"));
-        assert!(body.contains(
-            "Key_eigen_registered_stakes___quorum_number___4th__quorum_name___hello_Eigen__ 8"
-        ));
         assert!(body.contains("eigen_rpc_request_duration_seconds___method____eth_getBlockByNumber__client_version___rethv1_0_3__{quantile=\"1\"} 100"));
         assert!(body.contains("eigen_rpc_request_total___method___eth_getBlockByNumber__client_version___rethv1_0_3__ 10"));
     }
