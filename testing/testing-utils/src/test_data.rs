4a1c657ef811223425c4ebe8db11fd401530bb16
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-09-11 18:50:34 -0300

    removing unwrap in TestData::new

--- testing/testing-utils/src/test_data.rs
+++ testing/testing-utils/src/test_data.rs
@@ -13,11 +13,30 @@ pub struct TestData<T> {
 
 impl<T: DeserializeOwned> TestData<T> {
     /// Create a new instance of `TestData` with the given input data.
+    /// If the `TEST_DATA_PATH` environment variable is set, it reads the JSON file from the path,
+    /// otherwise (or if it fails) it uses the default input data.
+    ///
+    /// # Arguments
+    ///
+    /// * `default_input` - The default input data to use if the JSON file is not found or fails to read.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `TestData` with the input data read from the JSON file or the default input data.
     pub fn new(default_input: T) -> Self {
         if let Ok(path) = env::var("TEST_DATA_PATH") {
-            let file = File::open(path).unwrap();
+            let Ok(file) = File::open(path) else {
+                return TestData {
+                    input: default_input,
+                };
+            };
             let reader = BufReader::new(file);
-            serde_json::from_reader(reader).unwrap()
+            let Ok(ret) = serde_json::from_reader(reader) else {
+                return TestData {
+                    input: default_input,
+                };
+            };
+            ret
         } else {
             // use default values
             TestData {
