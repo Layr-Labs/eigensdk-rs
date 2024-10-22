74ec9a38ec86197e7241795d0dd79646b76f2f93
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-10-22 15:40:36 -0300

    Telemetry in OnceCell

--- crates/telemetry/src/telemetry_config.rs
+++ crates/telemetry/src/telemetry_config.rs
@@ -1,9 +1,28 @@
 use posthog_rs::{client, Client, Error, Event};
+use std::sync::OnceLock;
+
+static TELEMETRY_CELL: OnceLock<TelemetryConfig> = OnceLock::new();
+
+pub struct Telemetry {}
 
 pub struct TelemetryConfig {
     pub key: String,
     pub user_id: String,
     client: Client,
+}
+
+impl Telemetry {
+    pub fn new(key: &str, user_id: &str) -> Result<(), TelemetryConfig> {
+        let config = TelemetryConfig::new(key, user_id);
+        TELEMETRY_CELL.set(config)?;
+        Ok(())
+    }
+
+    pub fn capture_event(event: &str) -> Result<(), Error> {
+        let cell = TELEMETRY_CELL.get().unwrap();
+        let event = Event::new(event, &cell.user_id);
+        cell.client.capture(event)
+    }
 }
 
 impl TelemetryConfig {
@@ -15,10 +34,5 @@ impl TelemetryConfig {
             user_id: user_id.to_owned(),
             client,
         }
-    }
-
-    pub fn capture_event(&self, event: &str) -> Result<(), Error> {
-        let event = Event::new(event, &self.user_id);
-        self.client.capture(event)
     }
 }
