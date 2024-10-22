fd4d899934cb4bf1a44004d1376e2dd372ae4d59
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-10-22 15:54:19 -0300

    Reordering in a new file

---
+++ crates/telemetry/src/telemetry.rs
@@ -0,0 +1,18 @@
+use super::telemetry_config::TelemetryConfig;
+use posthog_rs::{Error, Event};
+
+pub struct Telemetry {}
+
+impl Telemetry {
+    pub fn new(key: &str, user_id: &str) -> Result<(), TelemetryConfig> {
+        let config = TelemetryConfig::new(key, user_id);
+        crate::TELEMETRY_CELL.set(config)?;
+        Ok(())
+    }
+
+    pub fn capture_event(event: &str) -> Result<(), Error> {
+        let cell = crate::TELEMETRY_CELL.get().unwrap();
+        let event = Event::new(event, &cell.user_id);
+        cell.client.capture(event)
+    }
+}
--- crates/telemetry/src/telemetry_config.rs
+++ crates/telemetry/src/telemetry_config.rs
@@ -1,32 +1,13 @@
-use posthog_rs::{client, Client, Error, Event};
-use std::sync::OnceLock;
-
-static TELEMETRY_CELL: OnceLock<TelemetryConfig> = OnceLock::new();
-
-pub struct Telemetry {}
+use posthog_rs::{client, Client};
 
 pub struct TelemetryConfig {
     pub key: String,
     pub user_id: String,
-    client: Client,
-}
-
-impl Telemetry {
-    pub fn new(key: &str, user_id: &str) -> Result<(), TelemetryConfig> {
-        let config = TelemetryConfig::new(key, user_id);
-        TELEMETRY_CELL.set(config)?;
-        Ok(())
-    }
-
-    pub fn capture_event(event: &str) -> Result<(), Error> {
-        let cell = TELEMETRY_CELL.get().unwrap();
-        let event = Event::new(event, &cell.user_id);
-        cell.client.capture(event)
-    }
+    pub(crate) client: Client,
 }
 
 impl TelemetryConfig {
-    pub fn new(key: &str, user_id: &str) -> Self {
+    pub(crate) fn new(key: &str, user_id: &str) -> Self {
         let client = client(key);
 
         Self {
