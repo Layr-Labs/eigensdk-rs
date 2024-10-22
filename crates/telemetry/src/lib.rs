a784c8002fd0f0d5fc67cf52f8bf42f1b2d98748
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-10-22 15:54:19 -0300

    Reordering in a new file

--- crates/telemetry/src/lib.rs
+++ crates/telemetry/src/lib.rs
@@ -1,1 +1,7 @@
+pub mod telemetry;
 pub mod telemetry_config;
+
+use std::sync::OnceLock;
+use telemetry_config::TelemetryConfig;
+
+static TELEMETRY_CELL: OnceLock<TelemetryConfig> = OnceLock::new();
