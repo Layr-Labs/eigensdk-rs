4fb2bb0181e09467e0f420bc39cdbb8e0be9370e
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-11-01 11:05:57 -0300

    remove get_transaction_status util

--- testing/testing-utils/src/transaction.rs
+++ testing/testing-utils/src/transaction.rs
@@ -1,36 +1,10 @@
 use alloy_primitives::FixedBytes;
-use alloy_provider::{
-    PendingTransactionBuilder, PendingTransactionError, Provider, ProviderBuilder,
-};
+use alloy_provider::{PendingTransactionBuilder, PendingTransactionError, ProviderBuilder};
 use alloy_rpc_types::eth::TransactionReceipt;
 use alloy_transport::TransportErrorKind;
-use eigen_utils::get_provider;
 use url::Url;
 
-/// Retrieves the status of a transaction from its hash.
-///
-/// # Arguments
-///
-/// `rpc_url` - The RPC URL.
-/// `tx_hash` - The hash of the transaction.
-///
-/// # Returns
-///
-/// A bool indicating wether the transaction was successful or not.
-pub async fn get_transaction_status(rpc_url: String, tx_hash: FixedBytes<32>) -> bool {
-    if wait_transaction(&rpc_url, tx_hash).await.is_err() {
-        return false;
-    }
-
-    get_provider(&rpc_url)
-        .get_transaction_receipt(tx_hash)
-        .await
-        .unwrap_or(None)
-        .map(|receipt| receipt.status())
-        .unwrap_or(false)
-}
-
-/// Wait for a transaction to finish.
+/// Wait for a transaction to finish and return its receipt.
 ///
 /// # Arguments
 ///
@@ -47,7 +21,5 @@ pub async fn wait_transaction(
     let url = Url::parse(rpc_url).map_err(|_| TransportErrorKind::custom_str("Invalid RPC URL"))?;
     let root_provider = ProviderBuilder::new().on_http(url);
     let pending_tx = PendingTransactionBuilder::new(&root_provider, tx_hash);
-    let receipt = pending_tx.get_receipt().await?;
-    debug_assert!(receipt.status());
-    Ok(receipt)
+    pending_tx.get_receipt().await
 }
