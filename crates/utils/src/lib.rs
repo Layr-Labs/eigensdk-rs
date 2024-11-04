fd283bdb4cdd7bb949974a7e81a8c4493bc4048d
supernovahs <91280922+supernovahs@users.noreply.github.com>
2024-11-05 02:21:37 +0530

    operator socket support in services (#156)
    closes #141
    
    ---------
    
    Co-authored-by: supernovahs <supernovahs@proton.me>

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -2,6 +2,7 @@
     html_logo_url = "https://github.com/Layr-Labs/eigensdk-rs/assets/91280922/bd13caec-3c00-4afc-839a-b83d2890beb5",
     issue_tracker_base_url = "https://github.com/Layr-Labs/eigensdk-rs/issues/"
 )]
+#![cfg(not(doctest))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![allow(unused_imports, clippy::all, rustdoc::all)]
 //! This module contains the sol! generated bindings for solidity contracts.
@@ -184,3 +185,5 @@ pub async fn get_ws_provider(
 /// Emitted when a new pubkey is registered
 pub const NEW_PUBKEY_REGISTRATION_EVENT: &str =
     "NewPubkeyRegistration(address,(uint256,uint256),(uint256[2],uint256[2]))";
+
+pub const OPERATOR_SOCKET_UPDATE: &str = "OperatorSocketUpdate(bytes32,string)";
