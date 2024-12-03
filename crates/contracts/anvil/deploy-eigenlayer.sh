edb4a3772562c00d9f4b9178f860bc8c1867145a
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-12-03 13:19:15 -0300

    fix env vars error on deploy scripts

--- crates/contracts/anvil/deploy-eigenlayer.sh
+++ crates/contracts/anvil/deploy-eigenlayer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-RPC_URL=http://localhost:8545
-PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+export RPC_URL=http://localhost:8545
+export PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
 # Navigate to the script directory
 parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
