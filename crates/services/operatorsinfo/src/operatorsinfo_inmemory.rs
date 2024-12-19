7ceea9cd9635b51788e2ba9cf14aa4e122fff552
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-12-19 17:31:22 -0300

    Fix operatorsinfo_inmemory tests

--- crates/services/operatorsinfo/src/operatorsinfo_inmemory.rs
+++ crates/services/operatorsinfo/src/operatorsinfo_inmemory.rs
@@ -430,7 +430,7 @@ mod tests {
     use eigen_logging::{get_logger, get_test_logger, init_logger};
     use eigen_testing_utils::anvil::start_anvil_container;
     use eigen_testing_utils::anvil_constants::{
-        get_avs_directory_address, get_delegation_manager_address,
+        get_allocation_manager_address, get_avs_directory_address, get_delegation_manager_address,
         get_operator_state_retriever_address, get_registry_coordinator_address,
         get_rewards_coordinator_address, get_strategy_manager_address,
     };
@@ -646,8 +646,10 @@ mod tests {
 
         let contract_delegation_manager =
             DelegationManager::new(delegation_manager_address, get_provider(&http_endpoint));
+
         let rewards_coordinator_address =
             get_rewards_coordinator_address(http_endpoint.clone()).await;
+
         let DelegationManager::permissionControllerReturn {
             _0: permission_controller,
         } = contract_delegation_manager
@@ -666,10 +668,14 @@ mod tests {
         );
         let signer = PrivateKeySigner::from_str(pvt_key).unwrap();
 
+        let allocation_manager = get_allocation_manager_address(http_endpoint.clone()).await;
+
         let el_chain_writer = ELChainWriter::new(
             delegation_manager_address,
             strategy_manager_address,
             rewards_coordinator_address,
+            permission_controller,
+            allocation_manager,
             el_chain_reader,
             http_endpoint.to_string(),
             pvt_key.to_string(),
