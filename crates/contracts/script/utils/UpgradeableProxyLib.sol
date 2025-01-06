0363cdddc44066a71abc4a94528312e8cf1ce3f2
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-01-06 14:59:23 -0300

    fix: update paths used in contracts

--- crates/contracts/script/utils/UpgradeableProxyLib.sol
+++ crates/contracts/script/utils/UpgradeableProxyLib.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.12;
 
 import {Vm} from "forge-std/Vm.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {TransparentUpgradeableProxy, ITransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {EmptyContract} from "@eigenlayer/test/mocks/EmptyContract.sol";
 
 library UpgradeableProxyLib {
@@ -27,7 +27,7 @@ library UpgradeableProxyLib {
 
     function upgrade(address proxy, address impl) internal {
         ProxyAdmin admin = getProxyAdmin(proxy);
-        admin.upgrade(TransparentUpgradeableProxy(payable(proxy)), impl);
+        admin.upgrade(ITransparentUpgradeableProxy(payable(proxy)), impl);
     }
 
     function upgradeAndCall(
@@ -37,7 +37,7 @@ library UpgradeableProxyLib {
     ) internal {
         ProxyAdmin admin = getProxyAdmin(proxy);
         admin.upgradeAndCall(
-            TransparentUpgradeableProxy(payable(proxy)),
+            ITransparentUpgradeableProxy(payable(proxy)),
             impl,
             initData
         );
