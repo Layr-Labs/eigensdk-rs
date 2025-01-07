cf589cd7898e66fa48a8b502fc8739afdd95b35f
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-01-07 15:03:56 -0300

    refactor: compute claim dynamically in `new_claim`

--- crates/chainio/clients/elcontracts/src/writer.rs
+++ crates/chainio/clients/elcontracts/src/writer.rs
@@ -725,7 +725,8 @@ mod test_utils {}
 #[cfg(test)]
 mod tests {
     use crate::test_utils::{
-        build_el_chain_reader, new_test_writer, ANVIL_FIRST_ADDRESS, ANVIL_FIRST_PRIVATE_KEY,
+        build_el_chain_reader, new_claim, new_test_writer, ANVIL_FIRST_ADDRESS,
+        ANVIL_FIRST_PRIVATE_KEY, OPERATOR_ADDRESS, OPERATOR_PRIVATE_KEY,
     };
     use alloy::providers::Provider;
     use alloy_primitives::{address, aliases::U96, Address, U256};
@@ -860,6 +861,26 @@ mod tests {
         let claimer = address!("5eb15C0992734B5e77c888D713b4FC67b3D679A2");
 
         let tx_hash = el_chain_writer.set_claimer_for(claimer).await.unwrap();
+
+        let receipt = wait_transaction(&http_endpoint, tx_hash).await.unwrap();
+        assert!(receipt.status());
+    }
+
+    #[tokio::test]
+    async fn test_process_claim() {
+        let (_container, http_endpoint, _ws_endpoint) = start_anvil_container().await;
+        let el_chain_writer = new_test_writer(
+            http_endpoint.to_string(),
+            ANVIL_FIRST_PRIVATE_KEY.to_string(),
+        )
+        .await;
+
+        let (_root, claim) = new_claim(&http_endpoint).await;
+
+        let tx_hash = el_chain_writer
+            .process_claim(claim, ANVIL_FIRST_ADDRESS)
+            .await
+            .unwrap();
 
         let receipt = wait_transaction(&http_endpoint, tx_hash).await.unwrap();
         assert!(receipt.status());
@@ -1129,9 +1150,8 @@ mod tests {
         let operator_set_id = 1;
         create_operator_set(http_endpoint.as_str(), avs_address, operator_set_id).await;
 
-        let operator_addr = address!("70997970C51812dc3A010C7d01b50e0d17dc79C8");
-        let operator_private_key =
-            "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+        let operator_addr = OPERATOR_ADDRESS;
+        let operator_private_key = OPERATOR_PRIVATE_KEY;
         let el_chain_writer =
             new_test_writer(http_endpoint.clone(), operator_private_key.to_string()).await;
         let bls_key = BlsKeyPair::new("1".to_string()).unwrap();
