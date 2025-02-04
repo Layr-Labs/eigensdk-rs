34bf91db1aef61b4234cce822413eb317902e211
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-02-04 10:56:09 -0300

    Add new test

--- crates/chainio/clients/avsregistry/src/writer.rs
+++ crates/chainio/clients/avsregistry/src/writer.rs
@@ -400,6 +400,30 @@ mod tests {
         test_deregister_operator(&avs_writer, quorum_nums, http_endpoint.clone()).await;
     }
 
+    #[tokio::test]
+    async fn test_set_rewards_initiator() {
+        let (_container, http_endpoint, _ws_endpoint) = start_anvil_container().await;
+
+        let private_key =
+            "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80".to_string();
+        let avs_writer =
+            build_avs_registry_chain_writer(http_endpoint.clone(), private_key.clone()).await;
+
+        let tx_hash = avs_writer
+            .set_rewards_initiator(
+                Address::from_str("a0Ee7A142d267C1f36714E4a8F75612F20a7972a").unwrap(),
+            )
+            .await
+            .unwrap();
+
+        let tx_status = wait_transaction(&http_endpoint, tx_hash)
+            .await
+            .unwrap()
+            .status();
+
+        assert!(tx_status);
+    }
+
     // this function is caller from test_avs_writer_methods
     async fn test_update_stake_of_operator_subset(
         avs_writer: &AvsRegistryChainWriter,
