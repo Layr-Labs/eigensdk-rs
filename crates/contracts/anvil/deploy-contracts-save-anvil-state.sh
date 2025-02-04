f698bd6aa330b79cd203279124ba5f47f3d6249d
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-02-04 16:42:31 -0300

    feat: add deploy-contracts-to-anvil-and-save-state target

---
+++ crates/contracts/anvil/deploy-contracts-save-anvil-state.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Enable the script to exit immediately if a command exits with a non-zero status
+set -o errexit -o nounset -o pipefail
+
+# cd to the directory of this script so that this can be run from anywhere
+anvil_dir=$(
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    pwd -P
+)
+cd $anvil_dir
+
+# Start anvil
+./dump-state.sh &
+anvil_pid=$!
+sleep 3
+
+# Deploy contracts
+./deploy-eigenlayer.sh
+./deploy-avs.sh
+
+# Stop anvil
+kill $anvil_pid
