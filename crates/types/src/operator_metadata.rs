9efe1462479ee8966ab645641c84d1030f5d7c36
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-02-17 13:53:14 -0300

    feat: add website checks

--- crates/types/src/operator_metadata.rs
+++ crates/types/src/operator_metadata.rs
@@ -1,3 +1,6 @@
+use std::path::{Path, PathBuf};
+
+use alloy::transports::http::reqwest::{self, Url};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
@@ -8,15 +11,15 @@ pub struct OperatorMetadata {
     /// Name of the operator
     name: String,
 
-    /// Website of the operator
-    website: String,
-
     /// Description of the operator. There is a 200-character limit
     description: String,
 
     /// Logo of the operator. This should be a link to a image file
     /// which is publicly accessible
     logo: String,
+
+    /// Website of the operator
+    website: Option<String>,
 
     /// Twitter handle of the operator (optional)
     twitter: Option<String>,
@@ -37,40 +40,91 @@ pub enum OperatorMetadataError {
     DescriptionTooLong,
     #[error("Description contains invalid characters")]
     DescriptionInvalid,
+
+    #[error("Logo cannot be empty")]
+    LogoUrlEmpty,
+    #[error("Logo url is invalid")]
+    LogoUrlInvalid,
+    #[error("Logo url has an invalid image extension")]
+    LogoUrlInvalidImageExtension,
+
+    #[error("Website url is invalid")]
+    WebsiteUrlInvalid,
+    #[error("Website url is longer than 1024 characters")]
+    WebsiteUrlTooLong,
+    #[error("Website url is points to local server")]
+    WebsiteUrlPointsToLocalServer,
 }
 
 impl OperatorMetadata {
     pub fn validate(&self) -> Result<(), OperatorMetadataError> {
-        // name is between 1-500 characters and matches the regex:
-        // `^[a-zA-Z0-9 +.,;:?!'’"“”\-_/()\[\]~&#$—%]+$`
+        // Alias the error types for brevity
+        use OperatorMetadataError::*;
+
         let text_regex = regex::Regex::new(r#"^[a-zA-Z0-9 +.,;:?!'’"“”\-_/()\[\]~&#$—%]+$"#)
             .expect("regex is valid");
+
+        // name must be non-empty, less than 500 characters, and match the regex
         if self.name.is_empty() {
-            return Err(OperatorMetadataError::NameEmpty);
+            return Err(NameEmpty);
         }
         if self.name.len() > 500 {
-            return Err(OperatorMetadataError::NameTooLong);
+            return Err(NameTooLong);
         }
-        if text_regex.is_match(&self.name) {
-            return Err(OperatorMetadataError::NameInvalid);
+        if !text_regex.is_match(&self.name) {
+            return Err(NameInvalid);
         }
-        // same for description
+
+        // description must be non-empty, less than 500 characters, and match the regex
         if self.description.is_empty() {
-            return Err(OperatorMetadataError::DescriptionEmpty);
+            return Err(DescriptionEmpty);
         }
         if self.description.len() > 500 {
-            return Err(OperatorMetadataError::DescriptionTooLong);
+            return Err(DescriptionTooLong);
         }
-        if text_regex.is_match(&self.description) {
-            return Err(OperatorMetadataError::DescriptionInvalid);
+        if !text_regex.is_match(&self.description) {
+            return Err(DescriptionInvalid);
         }
 
         // logo must be non-empty, must be a valid URL, end in .png,
         // and the server must return the content with a "image/png" mime type
+        if self.logo.is_empty() {
+            return Err(LogoUrlEmpty);
+        }
+        let Ok(url) = Url::parse(&self.logo) else {
+            return Err(LogoUrlInvalid);
+        };
+        let path = PathBuf::from(url.path());
+        if path.extension().map(|ext| ext == "png").unwrap_or(false) {
+            return Err(LogoUrlInvalidImageExtension);
+        }
+        // TODO: check if the server returns the content with a "image/png" mime type
 
         // website, if non-empty, must have less than 1024 characters,
         // not point to localhost or 127.0.0.1, and must be a valid URL
         // Also matches: `^(https?)://[^\s/$.?#].[^\s]*$`
+        if !self.website.as_ref().is_none_or(|s| s.is_empty()) {
+            let website = self.website.as_ref().unwrap();
+            if website.len() > 1024 {
+                return Err(WebsiteUrlTooLong);
+            }
+            let url = Url::parse(&website).ok().ok_or(WebsiteUrlInvalid)?;
+
+            let host = url.host_str().ok_or(WebsiteUrlInvalid)?;
+            if url.scheme().is_empty() || host.is_empty() {
+                return Err(WebsiteUrlInvalid);
+            }
+            if host == "localhost" || host == "127.0.0.1" {
+                return Err(WebsiteUrlPointsToLocalServer);
+            }
+
+            let website_regex =
+                regex::Regex::new(r#"^(https?)://[^\s/$.?#].[^\s]*$"#).expect("regex is valid");
+
+            if !website_regex.is_match(&website) {
+                return Err(WebsiteUrlInvalid);
+            }
+        }
 
         // twitter, if non-empty, must have less than 1024 characters,
         // not point to localhost or 127.0.0.1, and must be a valid URL
