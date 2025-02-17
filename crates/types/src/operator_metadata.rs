3cd5d9a251753cfc28ecfa13f0f066dcafcab727
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-02-17 13:24:53 -0300

    feat: add checks for name and description

--- crates/types/src/operator_metadata.rs
+++ crates/types/src/operator_metadata.rs
@@ -26,16 +26,44 @@ pub struct OperatorMetadata {
 pub enum OperatorMetadataError {
     #[error("Name cannot be empty")]
     NameEmpty,
+    #[error("Name has to be less than 500 characters long")]
+    NameTooLong,
+    #[error("Name contains invalid characters")]
+    NameInvalid,
+
+    #[error("Description cannot be empty")]
+    DescriptionEmpty,
+    #[error("Description has to be less than 500 characters long")]
+    DescriptionTooLong,
+    #[error("Description contains invalid characters")]
+    DescriptionInvalid,
 }
 
 impl OperatorMetadata {
     pub fn validate(&self) -> Result<(), OperatorMetadataError> {
         // name is between 1-500 characters and matches the regex:
         // `^[a-zA-Z0-9 +.,;:?!'’"“”\-_/()\[\]~&#$—%]+$`
+        let text_regex = regex::Regex::new(r#"^[a-zA-Z0-9 +.,;:?!'’"“”\-_/()\[\]~&#$—%]+$"#)
+            .expect("regex is valid");
         if self.name.is_empty() {
             return Err(OperatorMetadataError::NameEmpty);
         }
+        if self.name.len() > 500 {
+            return Err(OperatorMetadataError::NameTooLong);
+        }
+        if text_regex.is_match(&self.name) {
+            return Err(OperatorMetadataError::NameInvalid);
+        }
         // same for description
+        if self.description.is_empty() {
+            return Err(OperatorMetadataError::DescriptionEmpty);
+        }
+        if self.description.len() > 500 {
+            return Err(OperatorMetadataError::DescriptionTooLong);
+        }
+        if text_regex.is_match(&self.description) {
+            return Err(OperatorMetadataError::DescriptionInvalid);
+        }
 
         // logo must be non-empty, must be a valid URL, end in .png,
         // and the server must return the content with a "image/png" mime type
