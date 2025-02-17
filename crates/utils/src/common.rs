d46e438d93e85a98b7551a832f64930879a80851
Pablo Deymonnaz <deymonnaz@gmail.com>
2025-02-17 16:06:36 -0300

    comment in is_valid_ethereum_address, and check if address is Zero

--- crates/utils/src/common.rs
+++ crates/utils/src/common.rs
@@ -2,16 +2,29 @@ use regex::Regex;
 use reqwest::Error;
 use std::sync::OnceLock;
 
+static ZERO_ADDRESS: &str = "0x0000000000000000000000000000000000000000";
+
 pub async fn get_url_content(url: &str) -> Result<String, Error> {
     let response = reqwest::get(url).await?;
     response.text().await
 }
 
+/// Checks if the given string is a valid Ethereum address and it is not the zero address.
+///
+/// # Arguments
+///
+/// * `address` - A string slice that holds the Ethereum address.
+///
+/// # Returns
+///
+/// * `true` if the address is a valid Ethereum address and it is not the zero address.
+/// * `false` otherwise.
 pub fn is_valid_ethereum_address(address: &str) -> bool {
     static REGEX: OnceLock<Regex> = OnceLock::new();
     REGEX
         .get_or_init(|| Regex::new(r"(?i)^0x[0-9a-f]{40}$").expect("Fail to compile regex"))
         .is_match(address)
+        && address != ZERO_ADDRESS
 }
 
 #[cfg(test)]
