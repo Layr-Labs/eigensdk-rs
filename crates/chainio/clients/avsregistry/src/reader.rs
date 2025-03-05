1eee9f476b16f6e5d0585a5dba2bc45cb2a5201f
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-03-05 10:20:55 -0300

    test: add missing test for query_existing_registered_operator_pub_keys

--- crates/chainio/clients/avsregistry/src/reader.rs
+++ crates/chainio/clients/avsregistry/src/reader.rs
@@ -1684,4 +1684,49 @@ mod tests {
             .unwrap();
         assert_eq!(total_stake_indices_at_block_number.first(), Some(&1u32));
     }
+
+    #[tokio::test]
+    async fn test_query_existing_registered_operator_pub_keys() {
+        let (_container, http_endpoint, ws_endpoint) = start_m2_anvil_container().await;
+
+        let private_key = FIFTH_PRIVATE_KEY.to_string();
+        let avs_writer = build_avs_registry_chain_writer(http_endpoint.clone(), private_key).await;
+        let avs_reader = build_avs_registry_chain_reader(http_endpoint.clone()).await;
+
+        let bls_key_pair = BlsKeyPair::new(
+            "1371012690269088913462269866874713266643928125698382731338806296762673180359922"
+                .to_string(),
+        )
+        .unwrap();
+        let digest_hash: FixedBytes<32> = FixedBytes::from([0x02; 32]);
+        let quorum_nums = Bytes::from([0]);
+        let signature_expiry = U256::MAX;
+
+        let tx_hash = avs_writer
+            .register_operator_in_quorum_with_avs_registry_coordinator(
+                bls_key_pair.clone(),
+                digest_hash,
+                signature_expiry,
+                quorum_nums.clone(),
+                "".into(),
+            )
+            .await
+            .unwrap();
+        let tx_status = wait_transaction(&http_endpoint, tx_hash)
+            .await
+            .unwrap()
+            .status();
+        assert!(tx_status);
+
+        let operators = avs_reader
+            .query_existing_registered_operator_pub_keys(0, 1000, ws_endpoint)
+            .await
+            .unwrap();
+
+        assert_eq!(*operators.0.first().unwrap(), FIFTH_ADDRESS);
+        assert_eq!(
+            operators.1.first().unwrap().g1_pub_key,
+            BlsG1Point::new(bls_key_pair.public_key().g1()),
+        );
+    }
 }
