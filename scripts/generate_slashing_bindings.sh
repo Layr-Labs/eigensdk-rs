d5c684e25fd0494c8eee9818624bad53fc328de4
supernova <supernova@onlyevm.com>
2025-03-14 17:19:06 +0530

    add optimize and optimize runs in bindings

--- scripts/generate_slashing_bindings.sh
+++ scripts/generate_slashing_bindings.sh
@@ -63,16 +63,16 @@ cd $repo_root
 
 
 # Generate SDK bindings
-forge bind --alloy --use 0.8.27 --skip-build --bindings-path $SDK_BINDINGS_PATH --overwrite \
+forge bind --alloy --use 0.8.27 --optimize --optimizer-runs 200 --skip-build --bindings-path $SDK_BINDINGS_PATH --overwrite \
     --root $SDK_CONTRACTS_LOCATION --module \
     $SDK_CONTRACTS_ARGS
 
 # Generate middleware bindings
-forge bind --alloy --use 0.8.27 --skip-build --bindings-path $MIDDLEWARE_BINDINGS_PATH --overwrite \
+forge bind --alloy --use 0.8.27 --optimize --optimizer-runs 200 --skip-build --bindings-path $MIDDLEWARE_BINDINGS_PATH --overwrite \
     --root $MIDDLEWARE_CONTRACTS_LOCATION --module \
     $MIDDLEWARE_CONTRACTS_ARGS
 
 # Generate core bindings
-forge bind --alloy --use 0.8.27 --skip-build --bindings-path $CORE_BINDINGS_PATH --overwrite \
+forge bind --alloy --use 0.8.27 --optimize --optimizer-runs 200 --skip-build --bindings-path $CORE_BINDINGS_PATH --overwrite \
     --root $CORE_CONTRACTS_LOCATION --module \
     $CORE_CONTRACTS_ARGS
