4ff266cea39195bb473f6c1cdbc246d9c77a864d
supernova <supernova@onlyevm.com>
2025-04-01 10:50:21 +0530

    use chain_clients for common imports in tests

--- crates/services/operatorsinfo/src/operatorsinfo_onchain.rs
+++ crates/services/operatorsinfo/src/operatorsinfo_onchain.rs
@@ -88,152 +88,19 @@ impl OperatorInfoOnChain {
 
 #[cfg(test)]
 mod tests {
-    use alloy::{
-        primitives::{aliases::U96, Address},
-        providers::WalletProvider,
-        sol_types::SolCall,
-    };
-    use eigen_client_elcontracts::{reader::ELChainReader, writer::ELChainWriter};
-    use eigen_common::{get_provider, get_signer};
     use eigen_crypto_bls::BlsKeyPair;
-    use eigen_logging::get_test_logger;
     use eigen_testing_utils::{
         anvil::start_anvil_container,
         anvil_constants::{
-            get_allocation_manager_address, get_avs_directory_address,
-            get_bls_apk_registry_address, get_delegation_manager_address, get_erc20_mock_strategy,
-            get_registry_coordinator_address, get_rewards_coordinator_address,
-            get_service_manager_address, get_socket_registry_address, get_strategy_manager_address,
-            FIRST_PRIVATE_KEY, SECOND_ADDRESS, SECOND_PRIVATE_KEY,
+            get_bls_apk_registry_address, get_service_manager_address, get_socket_registry_address,
+            SECOND_ADDRESS, SECOND_PRIVATE_KEY,
         },
+        chain_clients::{build_el_chain_reader, create_operator_set, new_test_writer},
         transaction::wait_transaction,
     };
-    use eigen_utils::slashing::{
-        core::{
-            allocationmanager::AllocationManager::{self, OperatorSet},
-            delegationmanager::DelegationManager,
-        },
-        middleware::registrycoordinator::{
-            ISlashingRegistryCoordinatorTypes::OperatorSetParam,
-            IStakeRegistryTypes::StrategyParams, RegistryCoordinator,
-        },
-        sdk::mockavsservicemanager::MockAvsServiceManager,
-    };
+    use eigen_utils::slashing::core::allocationmanager::AllocationManager::OperatorSet;
 
     use crate::{operator_info::OperatorInfoService, operatorsinfo_onchain::OperatorInfoOnChain};
-
-    pub async fn build_el_chain_reader(http_endpoint: String) -> ELChainReader {
-        let delegation_manager_address =
-            get_delegation_manager_address(http_endpoint.clone()).await;
-        let avs_directory_address = get_avs_directory_address(http_endpoint.clone()).await;
-        let rewards_coordinator = get_rewards_coordinator_address(http_endpoint.clone()).await;
-
-        ELChainReader::build(
-            get_test_logger().clone(),
-            delegation_manager_address,
-            avs_directory_address,
-            rewards_coordinator,
-            &http_endpoint,
-        )
-        .await
-        .unwrap()
-    }
-
-    pub async fn new_test_writer(http_endpoint: String, private_key: String) -> ELChainWriter {
-        let el_chain_reader = build_el_chain_reader(http_endpoint.clone()).await;
-        let strategy_manager = get_strategy_manager_address(http_endpoint.clone()).await;
-        let rewards_coordinator = get_rewards_coordinator_address(http_endpoint.clone()).await;
-        let delegation_manager = get_delegation_manager_address(http_endpoint.clone()).await;
-        let allocation_manager = get_allocation_manager_address(http_endpoint.clone()).await;
-        let contract_delegation_manager =
-            DelegationManager::new(delegation_manager, get_provider(&http_endpoint));
-        let permission_controller = contract_delegation_manager
-            .permissionController()
-            .call()
-            .await
-            .unwrap()
-            ._0;
-        let registry_coordinator = get_registry_coordinator_address(http_endpoint.clone()).await;
-
-        ELChainWriter::new(
-            strategy_manager,
-            rewards_coordinator,
-            Some(permission_controller),
-            Some(allocation_manager),
-            registry_coordinator,
-            el_chain_reader,
-            http_endpoint.clone(),
-            private_key,
-        )
-    }
-
-    async fn create_operator_set(http_endpoint: &str, avs_address: Address) {
-        let allocation_manager_addr =
-            get_allocation_manager_address(http_endpoint.to_string()).await;
-        let default_signer = get_signer(FIRST_PRIVATE_KEY, http_endpoint);
-        let allocation_manager =
-            AllocationManager::new(allocation_manager_addr, default_signer.clone());
-        let registry_coordinator_addr =
-            get_registry_coordinator_address(http_endpoint.to_string()).await;
-        let service_manager_address = get_service_manager_address(http_endpoint.to_string()).await;
-        let service_manager =
-            MockAvsServiceManager::new(service_manager_address, default_signer.clone());
-        service_manager
-            .setAppointee(
-                default_signer.default_signer_address(),
-                allocation_manager_addr,
-                alloy::primitives::FixedBytes(AllocationManager::setAVSRegistrarCall::SELECTOR),
-            )
-            .send()
-            .await
-            .unwrap()
-            .get_receipt()
-            .await
-            .unwrap();
-        allocation_manager
-            .setAVSRegistrar(avs_address, registry_coordinator_addr)
-            .send()
-            .await
-            .unwrap()
-            .get_receipt()
-            .await
-            .unwrap();
-
-        // Create slashable quorum
-        let contract_registry_coordinator =
-            RegistryCoordinator::new(registry_coordinator_addr, default_signer.clone());
-        let operator_set_params = OperatorSetParam {
-            maxOperatorCount: 10,
-            kickBIPsOfOperatorStake: 100,
-            kickBIPsOfTotalStake: 1000,
-        };
-        let strategy = get_erc20_mock_strategy(http_endpoint.to_string()).await;
-        service_manager
-            .setAppointee(
-                registry_coordinator_addr,
-                allocation_manager_addr,
-                alloy::primitives::FixedBytes(AllocationManager::createOperatorSetsCall::SELECTOR),
-            )
-            .send()
-            .await
-            .unwrap()
-            .get_receipt()
-            .await
-            .unwrap();
-        let strategy_params = StrategyParams {
-            strategy,
-            multiplier: U96::from(1),
-        };
-
-        contract_registry_coordinator
-            .createSlashableStakeQuorum(operator_set_params, U96::from(0), vec![strategy_params], 0)
-            .send()
-            .await
-            .unwrap()
-            .get_receipt()
-            .await
-            .unwrap();
-    }
 
     async fn register_operator(http_endpoint: String) {
         let avs_address = get_service_manager_address(http_endpoint.clone()).await;
