b130a6e0abd60c009bb4de994ce07a536a5b7e9a
supernovahs <91280922+supernovahs@users.noreply.github.com>
2025-04-04 18:36:52 +0530

    chore: use common testing utils in bls_agg_test (#420)
    Fixes #
    
    ### What Changed?
    Moved create_quorum to testing utils, used create_operator_sets from
    utils instead of duplicate code
    ### Reviewer Checklist
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it

--- crates/eigen-cli/src/lib.rs
+++ crates/eigen-cli/src/lib.rs
@@ -290,11 +290,11 @@ mod test {
         let expected_addresses: ContractAddresses = serde_json::from_str(&format!(
             r#"{{
                 "avs": {{
-                    "bls-apk-registry": "0xc351628eb244ec633d5f21fbd6621e1a683b1181",
-                    "index-registry": "0xcbeaf3bde82155f56486fb5a1072cb8baaf547cc",
-                    "registry-coordinator": "0x7969c5ed335650692bc04293b07f5bf2e7a673c0",
-                    "service-manager": "0xb7278a61aa25c888815afc32ad3cc52ff24fe575",
-                    "stake-registry": "0x82e01223d51eb87e16a03e24687edf0f294da6f1"
+                    "bls-apk-registry": "0xfd471836031dc5108809d173a067e8486b9047a3",
+                    "index-registry": "0x1429859428c0abc9c2c47c8ee9fbaf82cfa0f20f",
+                    "registry-coordinator": "0x7bc06c482dead17c0e297afbc32f6e63d3846650",
+                    "service-manager": "0xcd8a1c3ba11cf5ecfa6267617243239504a98d90",
+                    "stake-registry": "0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3"
                 }},
                 "eigenlayer": {{
                     "delegation-manager": "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9",
@@ -329,11 +329,11 @@ mod test {
         let expected_addresses: ContractAddresses = serde_json::from_str(&format!(
             r#"{{
                 "avs": {{
-                    "bls-apk-registry": "0xc351628eb244ec633d5f21fbd6621e1a683b1181",
-                    "index-registry": "0xcbeaf3bde82155f56486fb5a1072cb8baaf547cc",
-                    "registry-coordinator": "0x7969c5ed335650692bc04293b07f5bf2e7a673c0",
+                    "bls-apk-registry": "0xfd471836031dc5108809d173a067e8486b9047a3",
+                    "index-registry": "0x1429859428c0abc9c2c47c8ee9fbaf82cfa0f20f",
+                    "registry-coordinator": "0x7bc06c482dead17c0e297afbc32f6e63d3846650",
                     "service-manager": "0x0000000000000000000000000000000000000000",
-                    "stake-registry": "0x82e01223d51eb87e16a03e24687edf0f294da6f1"
+                    "stake-registry": "0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3"
                 }},
                 "eigenlayer": {{
                     "delegation-manager": "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9",
