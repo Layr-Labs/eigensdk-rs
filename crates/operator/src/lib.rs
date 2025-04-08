d0e4a294ee04ec717fbf9b269b618dc8a8c623ef
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-04-08 15:36:18 -0300

    feat: build avsregistryreader and client inside the start

--- crates/operator/src/lib.rs
+++ crates/operator/src/lib.rs
@@ -10,6 +10,7 @@ use client::ClientAggregator;
 use eigen_aggregator::{SignedTaskResponse, TaskResponse};
 use eigen_client_avsregistry::reader::AvsRegistryChainReader;
 use eigen_crypto_bls::BlsKeyPair;
+use eigen_logging::logger::SharedLogger;
 use eigen_types::operator::OperatorId;
 use error::OperatorError;
 use futures_util::StreamExt;
@@ -52,15 +53,29 @@ impl<TP: OperatorTaskProcessor> Operator<TP> {
     /// # Returns
     ///
     /// * `Result<Self, OperatorError>` - The operator.
+    #[allow(clippy::too_many_arguments)]
     pub async fn new(
-        avs_registry_reader: &AvsRegistryChainReader,
         key_pair: &BlsKeyPair,
         operator_address: Address,
         operator_name: &str,
-        client_aggregator: &ClientAggregator,
+        logger: SharedLogger,
         ws_rpc_url: &str,
+        http_rpc_url: &str,
+        registry_coordiator_address: Address,
+        operator_state_retriever_address: Address,
+        aggregator_ip_port_address: String,
         task_processor: TP,
     ) -> Result<Self, OperatorError> {
+        let avs_registry_reader = AvsRegistryChainReader::new(
+            logger,
+            registry_coordiator_address,
+            operator_state_retriever_address,
+            http_rpc_url.to_string(),
+        )
+        .await?;
+
+        let client_aggregator = ClientAggregator::new(aggregator_ip_port_address).await?;
+
         let is_registered = avs_registry_reader
             .is_operator_registered(operator_address)
             .await
