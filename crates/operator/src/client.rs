c09a8c19c098d40f9b555b65f4ba946a7bdfbdc4
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-04-08 15:15:55 -0300

    feat(v2): add operator crate (#435)
    ### What Changed?
    This PR adds an `operator` crate to the SDK. Now you can start an
    operator, which will subscribe to `TaskEvents` and process them
    according to its implementation in the `OperatorTaskProcessor` trait.
    
    ### Reviewer Checklist
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it

---
+++ crates/operator/src/client.rs
@@ -0,0 +1,75 @@
+use eigen_aggregator::rpc_server::ProcessSignedTaskResponseClient;
+use serde::Serialize;
+use tarpc::tokio_serde::formats::Json;
+use tokio::time::{sleep, Duration};
+use tracing::{error, info};
+
+use crate::error::OperatorError;
+
+/// Client Aggregator
+#[derive(Debug, Clone)]
+pub struct ClientAggregator {
+    /// TARPC client to send requests to aggregator
+    pub client: ProcessSignedTaskResponseClient,
+}
+
+impl ClientAggregator {
+    /// Create a new client that connects to the aggregator RPC server
+    ///
+    /// # Arguments
+    ///
+    /// * `aggregator_ip_port_address` - The IP and port address of the aggregator
+    ///
+    /// # Returns
+    ///
+    /// * `Result<Self, OperatorError>` - The client aggregator
+    pub async fn new(aggregator_ip_port_address: String) -> Result<Self, OperatorError> {
+        let transport =
+            tarpc::serde_transport::tcp::connect(aggregator_ip_port_address, Json::default)
+                .await
+                .map_err(|_| OperatorError::TransportError)?;
+        let client =
+            ProcessSignedTaskResponseClient::new(tarpc::client::Config::default(), transport)
+                .spawn();
+
+        Ok(Self { client })
+    }
+
+    /// Send signed task response to the aggregator with exponential backoff
+    ///
+    /// # Arguments
+    ///
+    /// * `signed_task_response` - The signed task response to send
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), OperatorError>` - The result of the operation
+    pub async fn send_signed_task_response(
+        &self,
+        signed_task_response: impl Serialize,
+    ) -> Result<(), OperatorError> {
+        let mut delay = Duration::from_secs(1);
+
+        for _ in 0..5 {
+            let signed_task_string = serde_json::to_string(&signed_task_response)?;
+            let ctx = tarpc::context::current();
+            let response = self
+                .client
+                .process_signed_task_response(ctx, signed_task_string)
+                .await?;
+
+            if response.is_ok() {
+                info!("Signed task response sent to aggregator");
+                return Ok(());
+            }
+
+            // Exponential backoff
+            info!("Retrying in {} seconds...", delay.as_secs());
+            sleep(delay).await;
+            delay *= 2; // Double the delay for the next retry
+        }
+        error!("Could not send signed task response to aggregator. Tried 5 times.");
+
+        Err(OperatorError::MaxRetryExceeded)
+    }
+}
---
+++ crates/operator/src/error.rs
@@ -0,0 +1,37 @@
+// use eigen_config::error::ConfigError;
+use eigen_crypto_bls::error::BlsError;
+use rust_bls_bn254::errors::KeystoreError;
+use tarpc::client::RpcError;
+use thiserror::Error;
+
+/// Error returned by AvsRegistry
+#[derive(Debug, Error)]
+pub enum OperatorError {
+    /// Operator Registration Error
+    #[error("Failed to register operator")]
+    RegistrationError,
+    /// Operator Id Error
+    #[error("Failed to get operator id")]
+    OperatorIdError,
+    /// Operator Subscribe Logs Error
+    #[error("Failed to subscribe logs")]
+    SubscribeLogsError,
+    /// Operator Transport Error
+    #[error("Could not connect")]
+    TransportError,
+    /// Bls Keystore error
+    #[error("Bls Keystore error ")]
+    BlsKeystoreError(#[from] KeystoreError),
+    /// Bls crate(SDK) error
+    #[error("Bls crate(SDK) error")]
+    EigenBlsError(#[from] BlsError),
+    /// Failed to send signed task response
+    #[error("Failed to send signed task response")]
+    SendSignedTaskResponseError(#[from] RpcError),
+    /// Failed to serialize signed task response
+    #[error("Failed to serialize signed task response")]
+    SerializationError(#[from] serde_json::Error),
+    /// Max retry attempts exceeded
+    #[error("Max retry attempts exceeded")]
+    MaxRetryExceeded,
+}
