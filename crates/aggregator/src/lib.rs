a399c8dd2560cd74a0ccd9fa28c16fc0aee86ebb
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-04-14 14:23:28 -0300

    refactor(v2): use `ws_rpc_url` from config instead of passing it as a parameter in Aggregator (#457)
    ### What Changed?
    
    This PR removes the parameter that the `start()` function receives since
    we already have this value in `AggregatorConfig`.
    ### Reviewer Checklist
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it

--- crates/aggregator/src/lib.rs
+++ crates/aggregator/src/lib.rs
@@ -49,6 +49,7 @@ pub struct Aggregator<TP> {
     task_processor: Arc<Mutex<TP>>,
     service_handle: ServiceHandle,
     aggregated_response_receiver: AggregateReceiver,
+    ws_rpc_url: String,
 }
 
 impl<TP: TaskProcessor + Send + Sync + 'static + Clone> Aggregator<TP> {
@@ -101,6 +102,7 @@ impl<TP: TaskProcessor + Send + Sync + 'static + Clone> Aggregator<TP> {
             task_processor: Arc::new(Mutex::new(task_processor)),
             service_handle,
             aggregated_response_receiver,
+            ws_rpc_url: config.ws_rpc_url,
         })
     }
 
@@ -111,14 +113,10 @@ impl<TP: TaskProcessor + Send + Sync + 'static + Clone> Aggregator<TP> {
     /// - process_tasks: receives tasks from the log and processes them
     /// - process_aggregated_signatures: processes the aggregated signatures
     ///
-    /// # Arguments
-    ///
-    /// * `ws_rpc_url` - The websocket RPC URL
-    ///
     /// # Returns
     ///
     /// * `Result<(), AggregatorError>` - The result of the operation
-    pub async fn start(self, ws_rpc_url: String) -> Result<(), AggregatorError> {
+    pub async fn start(self) -> Result<(), AggregatorError> {
         info!("Starting aggregator");
 
         let task_processor = self.task_processor.clone();
@@ -131,10 +129,9 @@ impl<TP: TaskProcessor + Send + Sync + 'static + Clone> Aggregator<TP> {
             task_processor.clone(),
             service_handle.clone(),
         ));
-        let task_processor = self.task_processor.clone();
 
         let process_handle = tokio::spawn(Self::process_tasks(
-            ws_rpc_url,
+            self.ws_rpc_url,
             task_processor.clone(),
             service_handle,
         ));
