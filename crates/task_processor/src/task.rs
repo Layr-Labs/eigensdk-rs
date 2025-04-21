053bb4df172182e0c79db38eb84ceb54da587fc9
Pablo Deymonnaz <deymonnaz@gmail.com>
2025-04-21 18:10:04 -0300

    Task abstraction in task processor refactor

---
+++ crates/task_processor/src/task.rs
@@ -0,0 +1,16 @@
+/// Task abstraction
+#[allow(non_snake_case)]
+#[derive(Debug, Clone)]
+pub struct Task<INPUT>
+where
+    INPUT: Clone,
+{
+    #[allow(missing_docs)]
+    pub input: INPUT,
+    #[allow(missing_docs)]
+    pub task_created_block: u32,
+    #[allow(missing_docs)]
+    pub quorum_numbers: alloy::sol_types::private::Bytes,
+    #[allow(missing_docs)]
+    pub quorum_threshold_percentage: u8,
+}
