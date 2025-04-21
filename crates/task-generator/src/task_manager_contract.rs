2a4a55ed6aa04fb2bc545900d1ad1c4ce95391c2
Pablo Deymonnaz <deymonnaz@gmail.com>
2025-04-21 16:58:17 -0300

    receive_event skeleton in TaskManagerContract

--- crates/task-generator/src/task_manager_contract.rs
+++ crates/task-generator/src/task_manager_contract.rs
@@ -2,10 +2,16 @@ use alloy::dyn_abi::SolType;
 
 #[allow(missing_docs)]
 pub trait TaskManagerContract<T: SolType, R: SolType, N: SolType> {
+    type Event;
+
     fn create_new_task(&self, task: T);
 
     fn respond_to_task(&self, task: T, response: R, non_signer_stakes_and_signature: N) {
         todo!("Implement respond_to_task")
+    }
+
+    fn receive_event() -> Result<Self::Event, ()> {
+        todo!("Implement receive_event")
     }
 
     // fn submit_challenge(&self) ->
