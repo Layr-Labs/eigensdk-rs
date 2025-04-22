a89056515557e750bac9ea98a88523bfec746a14
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-04-22 17:40:03 -0300

    feat(v2): new version of operator (#460)
    ### What Changed?
    
    - Removed the `TaskProcessor` trait from `Operator`.  
    - `Operator::start` now accepts any event type (`SolEvent`) and any
    task-response type (`SolType` + `SolValue`), making it fully generic.
    - Now `start()` receives a function as a parameter, where the user
    should put their business logic to respond to a task.
    
    ### Reviewer Checklist
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it

--- examples/operator/src/main.rs
+++ examples/operator/src/main.rs
@@ -1,55 +1,24 @@
 #![allow(missing_docs)]
 
-use alloy::{
-    primitives::{address, keccak256, B256, U256},
-    sol_types::SolValue,
+use alloy::primitives::address;
+use bindings::iincrediblesquaringtaskmanager::IIncredibleSquaringTaskManager::{
+    NewTaskCreated, TaskResponse,
 };
-use bindings::iincrediblesquaringtaskmanager::IIncredibleSquaringTaskManager;
-use eigen_aggregator::TaskResponse;
 use eigen_crypto_bls::BlsKeyPair;
 use eigen_logging::get_logger;
-use eigen_operator::{operator_task_processor::OperatorTaskProcessor, Operator};
+use eigen_operator::{error::OperatorError, Operator};
 use eigen_testing_utils::anvil_constants::{FIRST_ADDRESS, OPERATOR_BLS_KEY};
-use serde::{Deserialize, Serialize};
 
 // Allow warnings in auto-generated code
 #[allow(warnings)]
 pub mod bindings;
 
-// 1. Implement the `TaskResponse` trait
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct ExampleTaskResponse {
-    task_index: u32,
-    number_squared: U256,
-}
-
-impl TaskResponse for ExampleTaskResponse {
-    fn digest(&self) -> B256 {
-        let response = IIncredibleSquaringTaskManager::TaskResponse {
-            referenceTaskIndex: self.task_index,
-            numberSquared: self.number_squared,
-        };
-        keccak256(response.abi_encode())
-    }
-
-    fn task_index(&self) -> u32 {
-        self.task_index
-    }
-}
-
-// 2. Implement the OperatorTaskProcessor trait
-struct OperatorTaskProcessorImpl;
-
-impl OperatorTaskProcessor for OperatorTaskProcessorImpl {
-    type NewTaskEvent = IIncredibleSquaringTaskManager::NewTaskCreated;
-    type TaskResponse = ExampleTaskResponse;
-
-    fn process_new_task(&self, new_task_created: Self::NewTaskEvent) -> Self::TaskResponse {
-        ExampleTaskResponse {
-            task_index: new_task_created.taskIndex,
-            number_squared: new_task_created.task.numberToBeSquared,
-        }
-    }
+fn square(event: NewTaskCreated) -> Result<TaskResponse, OperatorError> {
+    let square = event.task.numberToBeSquared * event.task.numberToBeSquared;
+    Ok(TaskResponse {
+        referenceTaskIndex: event.taskIndex,
+        numberSquared: square,
+    })
 }
 
 // This example shows how to initialize an operator and start to listen for new task events.
@@ -63,7 +32,6 @@ async fn main() {
     let ws_rpc_url = "ws://localhost:8545".to_string();
     let logger = get_logger();
 
-    let operator_task_processor = OperatorTaskProcessorImpl;
     let bls_key_pair = BlsKeyPair::new(OPERATOR_BLS_KEY.to_string()).unwrap();
 
     // Initialize the operator
@@ -77,12 +45,11 @@ async fn main() {
         registry_coordiator_address,
         operator_state_retriever_address,
         server_address,
-        operator_task_processor,
     )
     .await
     .unwrap();
 
     // Subscribe to the new task events and start listening. When a new task is created,
     // the operator will process it and send the signed task response to the aggregator.
-    operator.start().await.unwrap();
+    operator.start(square).await.unwrap();
 }
