a2579e63941d1ed2616b126f1744d6c3b06b0816
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-05-05 11:22:55 -0300

    feat: add task_spammer to example

--- examples/incredible-squaring/src/lib.rs
+++ examples/incredible-squaring/src/lib.rs
@@ -13,17 +13,18 @@ use bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::Incr
 use bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::NewTaskCreated;
 use bindings::incrediblesquaringtaskmanager::BN254::{G1Point, G2Point};
 use eigen_task_processor::{
-    task::Task,
-    task_manager::{TaskManagerContract, TaskManagerError},
-    task_response::TaskResponse,
+    task::Task, task_manager::TaskManagerError, task_response::TaskResponse,
 };
+use eigen_task_spammer::error::TaskSpammerError;
+use eigen_types::operator::{QuorumNum, QuorumThresholdPercentage};
 use eigen_utils::slashing::middleware::iblssignaturechecker::IBLSSignatureCheckerTypes::NonSignerStakesAndSignature;
 
 // Allow warnings in auto-generated code
 #[allow(warnings)]
 pub mod bindings;
 
-impl<T, P, N> TaskManagerContract<T, P, N> for IncredibleSquaringTaskManagerInstance<T, P, N>
+impl<T, P, N> eigen_task_processor::task_manager::TaskManagerContract<T, P, N>
+    for IncredibleSquaringTaskManagerInstance<T, P, N>
 where
     T: Transport + Clone + Send + Sync,
     P: Provider<T, N>,
@@ -101,5 +102,38 @@ where
         .unwrap();
 
         Ok(())
+    }
+}
+
+// Implement the TaskManagerContract trait for the task manager contract.
+// You need to specify the input type of the task. In this case, U256.
+// You also need to specify the call type of the task manager contract. `createNewTask` uses `createNewTaskCall`.
+// You also need to specify the provider and network types.
+//
+// NOTE: When you are implementing this, you will have an exteranl trait `TaskManagerContract` and and external struct
+// `CONTRACT_NAME_INSTANCE`, so it will throw an error. You can wrap the external struct in a newtype to avoid this.
+// Example:
+// struct TaskManagerWrapper<T, P, N>(IncredibleSquaringTaskManagerInstance<T, P, N>);
+//
+// impl<T, P, N> TaskManagerContract<U256, T, P, N> for TaskManagerWrapper<T, P, N> { ... }
+impl<T, P, N> eigen_task_spammer::task_manager::TaskManagerContract<U256, T, P, N>
+    for IncredibleSquaringTaskManagerInstance<T, P, N>
+where
+    T: Transport + Clone + Send + Sync,
+    P: Provider<T, N>,
+    N: Network,
+{
+    async fn create_new_task(
+        &self,
+        input: U256,
+        quorum_threshold: QuorumThresholdPercentage,
+        quorums: Vec<QuorumNum>,
+    ) -> Result<N::ReceiptResponse, TaskSpammerError> {
+        Ok(self
+            .createNewTask(input, quorum_threshold.into(), quorums.into())
+            .send()
+            .await?
+            .get_receipt()
+            .await?)
     }
 }
