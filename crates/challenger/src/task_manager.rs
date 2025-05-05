6cb025364afc0a4a358500f23c85cf8fe22ca6c8
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-05 09:08:25 -0300

    refactor: use new sol! struct

--- crates/challenger/src/task_manager.rs
+++ crates/challenger/src/task_manager.rs
@@ -4,7 +4,7 @@ use eigen_utils::slashing::middleware::iblssignaturechecker::BN254::G1Point; // 
 use serde::de::DeserializeOwned;
 use std::{fmt::Debug, future::Future};
 
-use crate::challenger_processor::TaskResponseData;
+use crate::challenger_processor::TaskResponseMetadataSol;
 
 /// Error returned by the task processor
 pub type TaskManagerError = Box<dyn core::error::Error + Send>;
@@ -44,7 +44,7 @@ pub trait TaskManagerContract<T, P, N: alloy::network::Network> {
         &self,
         task: Task<Self::Input>,
         task_response: TaskResponse<Self::Output>,
-        task_response_metadata: TaskResponseData<Self::Output>,
+        task_response_metadata: TaskResponseMetadataSol,
         pubkeys_of_non_signing_operators: Vec<G1Point>,
     ) -> impl Future<Output = Result<(), TaskManagerError>> + Send;
 }
