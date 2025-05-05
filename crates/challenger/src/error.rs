6d71234ab8909f4776db0d40fc8d52299b745b0a
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-05 09:06:51 -0300

    feat: add new challenger errors

--- crates/challenger/src/error.rs
+++ crates/challenger/src/error.rs
@@ -2,6 +2,8 @@ use alloy::contract::Error as AlloyError;
 use alloy::sol_types::Error as AlloySolTypeError;
 use thiserror::Error;
 
+use crate::task_manager::TaskManagerError;
+
 /// Error returned by chainio
 #[derive(Debug, Error)]
 pub enum ChallengerError {
@@ -16,18 +18,6 @@ pub enum ChallengerError {
     /// Alloy sol types error
     #[error("Alloy sol types error :{0}")]
     AlloySolType(#[from] AlloySolTypeError),
-
-    /// Task Response is correct
-    #[error("Task Response is not wrong")]
-    TaskResponseIsCorrect,
-
-    /// Task Response not found
-    #[error("Task Response not found")]
-    TaskResponseNotFound,
-
-    /// Task not found
-    #[error("Task not found")]
-    TaskNotFound,
 
     /// Transaction hash not found
     #[error("Tx hash not found")]
@@ -44,4 +34,20 @@ pub enum ChallengerError {
     /// Failed to parse ECDSA keystore signer
     #[error("Failed to parse ecdsa keystore signer")]
     ECDSAKeystoreSigner,
+
+    /// Transaction not found
+    #[error("Transaction {0} not found")]
+    TransactionNotFound(String),
+
+    /// Task Manager error
+    #[error("Task Manager error")]
+    TaskManagerError(#[from] TaskManagerError),
+
+    /// Task Index missing in topics
+    #[error("Task Index missing in topics")]
+    TaskIndexMissingInTopics,
+
+    /// Invalid task index conversion
+    #[error("Invalid task index conversion")]
+    InvalidTaskIndexConversion,
 }
