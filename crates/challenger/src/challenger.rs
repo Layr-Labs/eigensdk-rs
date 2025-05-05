4b8a397430bff726eec5ddc964f2a3bf028d8049
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-05 16:16:20 -0300

    docs: add documentation to Challenger trait

--- crates/challenger/src/challenger.rs
+++ crates/challenger/src/challenger.rs
@@ -22,16 +22,33 @@ pub trait ChallengerTaskProcessor {
     type Output: Clone + SolValue + Send + Sync + 'static;
 
     /// Handle a new task creation when a new task event is received
+    ///
+    /// # Arguments
+    ///
+    /// * `log` - The log of the new task creation
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), ChallengerError>` - The result of the operation
     fn handle_task_creation(
         &mut self,
         log: Log,
     ) -> impl Future<Output = Result<(), ChallengerError>> + Send;
 
     /// Handle a task response when a task response event is received
+    ///
+    /// # Arguments
+    ///
+    /// * `log` - The log of the task response
+    /// * `is_response_correct` - The logic to check if the response is correct
+    ///
+    /// # Returns
+    ///
+    /// * `Result<(), ChallengerError>` - The result of the operation
     fn handle_task_response(
         &mut self,
         log: Log,
-        check_response: impl Fn(Task<Self::Input>, TaskResponse<Self::Output>) -> Result<bool, ChallengerError>
+        is_response_correct: impl Fn(Task<Self::Input>, TaskResponse<Self::Output>) -> Result<bool, ChallengerError>
             + Send,
     ) -> impl Future<Output = Result<(), ChallengerError>> + Send;
 }
