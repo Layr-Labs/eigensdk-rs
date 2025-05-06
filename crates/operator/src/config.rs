2fe1946a22538490bb34a25a23c7d939ca4eee43
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-05-06 12:25:40 -0300

    docs: add documentation

--- crates/operator/src/config.rs
+++ crates/operator/src/config.rs
@@ -4,12 +4,22 @@ use eigen_crypto_bls::BlsKeyPair;
 #[derive(Debug, Clone)]
 /// Operator configuration struct
 pub struct OperatorConfig {
+    /// BLS key pair of the operator
     pub bls_key_pair: BlsKeyPair,
+    /// Address of the operator
     pub operator_address: Address,
+    /// Name of the operator
+    /// This is used for logging and debugging purposes.
     pub operator_name: String,
+    /// Ethereum WebSocket RPC URL
     pub ws_rpc_url: String,
+    /// Ethereum HTTP RPC URL
     pub http_rpc_url: String,
+    /// Address of the registry coordinator
+    /// Used to check the operator is registered.
     pub registry_coordinator_address: Address,
+    /// Address of the operator state retriever
     pub operator_state_retriever_address: Address,
+    /// IP and port of the aggregator
     pub aggregator_ip_port: String,
 }
