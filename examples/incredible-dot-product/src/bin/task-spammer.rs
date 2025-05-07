51c01d478a186067486486d1cadf41e61f2fc126
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-07 13:17:37 -0300

    refactor: move logic to general trait

--- examples/incredible-dot-product/src/bin/task-spammer.rs
+++ examples/incredible-dot-product/src/bin/task-spammer.rs
@@ -1,22 +1,11 @@
+use alloy::primitives::U256;
+use eigensdk::{common::get_signer, task_spammer::TaskSpammerBuilder};
+use incredible_bindings::incredibledotproducttaskmanager::{
+    IIncredibleDotProductTaskManager::DotProductInput,
+    IncredibleDotProductTaskManager::IncredibleDotProductTaskManagerInstance,
+};
+use incredible_dot_product::{config::Config, TaskManagerWrapper};
 use std::time::Duration;
-
-use alloy::{
-    contract::private::{Provider, Transport},
-    network::Network,
-    primitives::U256,
-};
-use eigensdk::{
-    common::get_signer,
-    task_spammer::{
-        error::TaskSpammerError, task_manager::TaskManagerContract, TaskSpammerBuilder,
-    },
-    types::operator::{QuorumNum, QuorumThresholdPercentage},
-};
-use incredible_bindings::incrediblesquaringtaskmanager::{
-    IIncredibleSquaringTaskManager::DotProductInput,
-    IncredibleSquaringTaskManager::IncredibleSquaringTaskManagerInstance,
-};
-use incredible_dot_product::config::Config;
 
 #[tokio::main]
 async fn main() {
@@ -24,13 +13,13 @@ async fn main() {
     let wallet = get_signer(&config.rpc_config.signer, &config.rpc_config.http_rpc_url);
 
     let contract =
-        IncredibleSquaringTaskManagerInstance::new(config.contract_address.task_manager, wallet);
+        IncredibleDotProductTaskManagerInstance::new(config.contract_address.task_manager, wallet);
     let wrapper = TaskManagerWrapper(contract);
 
     TaskSpammerBuilder::new(wrapper)
         .with_iter((0..).map(|i| DotProductInput {
-            a: vec![U256::from(i); 4],
-            b: vec![U256::from(i * 2); 4],
+            X: vec![U256::from(i); 4],
+            Y: vec![U256::from(i * 2); 4],
         }))
         .with_quorum(50, vec![0])
         .with_interval(Duration::from_secs(10))
