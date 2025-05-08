2fceea07c66ecb8c680b9e59750142bd17686df7
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-08 14:34:01 -0300

    refactor: remove spaces

--- examples/incredible-dot-product/src/lib.rs
+++ examples/incredible-dot-product/src/lib.rs
@@ -2,15 +2,12 @@
 //!
 //! This example demonstrates how to use the SDK to build an AVS.
 
-/// Task manager for the Incredible Dot Product Operator
-pub mod task_manager;
-
 /// Bindings for the Incredible Dot Product Operator
 pub mod bindings;
-
+/// Task manager for the Incredible Dot Product Operator
+pub mod task_manager;
 /// Utils for operator registry
 pub mod utils;
 
 pub use bindings::incredibledotproducttaskmanager::*;
-/// Re-export the task manager and utils
 pub use utils::*;
