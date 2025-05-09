530e0fcf038c89979c8ccf369c5d951e25e2668f
Pablo Deymonnaz <deymonnaz@gmail.com>
2025-05-09 14:19:58 -0300

    feat: add socket to OperatorInfo struct (#463) (#464)
    This PR adds a missing field `socket` to `OperatorInfo`. We also added a
    method `get_operator_socket()` in `AvsRegistryServiceChainCaller`.
    
    The new method is used in `get_operators_avs_state_at_block()`, now you
    can obtain the operator socket value that is inside the `OperatorInfo`.
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it
    
    Fixes #
    
    <!-- Describe the changes made in this pull request -->
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it
    
    ---------
    
    Co-authored-by: Damian Ramirez <damian.ramirez@lambdaclass.com>
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- crates/services/avsregistry/src/lib.rs
+++ crates/services/avsregistry/src/lib.rs
@@ -54,7 +54,7 @@
 //!#     };
 //!#     let operator_address = Address::from(FIRST_ADDRESS);
 //!#     let avs_registry = FakeAvsRegistryReader::new(test_operator.clone(), operator_address);
-//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone());
+//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone(), Some(String::from("test_socket")));
 //!#
 //!     let avs_registry_service =
 //!         AvsRegistryServiceChainCaller::new(avs_registry, operator_info_service);
@@ -89,7 +89,7 @@
 //!#     };
 //!#     let operator_address = Address::from(FIRST_ADDRESS);
 //!#     let avs_registry = FakeAvsRegistryReader::new(test_operator.clone(), operator_address);
-//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone());
+//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone(), Some(String::from("test_socket")));
 //!#
 //!#     let avs_registry_service =
 //!#         AvsRegistryServiceChainCaller::new(avs_registry, operator_info_service);
@@ -128,7 +128,7 @@
 //!#     };
 //!#     let operator_address = Address::from(FIRST_ADDRESS);
 //!#     let avs_registry = FakeAvsRegistryReader::new(test_operator.clone(), operator_address);
-//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone());
+//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone(), Some(String::from("test_socket")));
 //!#
 //!#     let avs_registry_service =
 //!#         AvsRegistryServiceChainCaller::new(avs_registry, operator_info_service);
@@ -168,7 +168,7 @@
 //!#     };
 //!#     let operator_address = Address::from(FIRST_ADDRESS);
 //!#     let avs_registry = FakeAvsRegistryReader::new(test_operator.clone(), operator_address);
-//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone());
+//!#     let operator_info_service = FakeOperatorInfoService::new(test_operator.bls_keypair.clone(), Some(String::from("test_socket")));
 //!#
 //!#     let avs_registry_service =
 //!#         AvsRegistryServiceChainCaller::new(avs_registry, operator_info_service);
