40ccf84aaadd9c374a421e4aef852d096e0a869d
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-05-12 16:49:43 -0300

    feat(v2): split `TaskManager` trait and simplify macros (#487)
    Fixes #
    
    ### What Changed?
    
    This PR splits the `TaskManager` trait into `TaskManagerDefs`, which
    includes type definitions and constants, and the `TaskManager` trait
    which requires the `Defs` and implements the other methods.
    
    This simplifies the macro by reducing its parameters to a type
    implementing `TaskManagerDefs`, and another which is an Alloy contract
    instance.
    
    ### Reviewer Checklist
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it

--- examples/incredible-squaring/src/lib.rs
+++ examples/incredible-squaring/src/lib.rs
@@ -1,30 +1,28 @@
 //! Example AVS which squares a number
 
+use alloy::primitives::B256;
 use alloy::primitives::U256;
+use alloy::sol_types::SolEvent;
 use bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::IncredibleSquaringTaskManagerInstance;
 use bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::NewTaskCreated;
 use bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::TaskResponded;
-use eigen_task_processor::impl_task_manager;
+use eigen_task_processor::impl_task_manager_from_defs_and_contract;
+use eigen_task_processor::task_manager::TaskManagerDefs;
 
 // Allow warnings in auto-generated code
 #[allow(warnings)]
 pub mod bindings;
 
-// Implement the TaskManager trait for the task manager contract.
-// You need to specify the input type of the task. In this case, U256.
-// You also need to specify the call type of the task manager contract. `createNewTask` uses `createNewTaskCall`.
-// You also need to specify the provider and network types.
-//
-// NOTE: When you are implementing this, you will have an external trait `TaskManager` and an external struct
-// `CONTRACT_NAME_INSTANCE`, so it will throw an error. You can wrap the external struct in a newtype to avoid this.
-// Example:
-// struct TaskManagerWrapper<T, P, N>(IncredibleSquaringTaskManagerInstance<T, P, N>);
-//
-// impl<T, P, N> TaskManager<U256, T, P, N> for TaskManagerWrapper<T, P, N> { ... }
-impl_task_manager!(
-    Contract = IncredibleSquaringTaskManagerInstance,
-    Input = U256,
-    Output = U256,
-    NewTaskEvent = NewTaskCreated,
-    TaskRespondedEvent = TaskResponded,
-);
+// Implement the [`TaskManagerDefs`] trait for a unit struct.
+// You need to specify the input and output types of the task. In this case, U256.
+// You also need to specify the selectors for the new task event and the task responded event.
+pub struct ISTaskManager;
+
+impl TaskManagerDefs for ISTaskManager {
+    type Input = U256;
+    type Output = U256;
+    const NEW_TASK_EVENT_SELECTOR: B256 = NewTaskCreated::SIGNATURE_HASH;
+    const TASK_RESPONDED_EVENT_SELECTOR: B256 = TaskResponded::SIGNATURE_HASH;
+}
+
+impl_task_manager_from_defs_and_contract!(ISTaskManager => IncredibleSquaringTaskManagerInstance);
