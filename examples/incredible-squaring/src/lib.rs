b5656a56b8b8d566f709253c933e0872eb0716b7
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-13 12:03:01 -0300

    feat: use future in operator

--- examples/incredible-squaring/src/lib.rs
+++ examples/incredible-squaring/src/lib.rs
@@ -18,6 +18,13 @@ pub fn square(_task_index: u32, number_to_be_squared: U256) -> Result<U256, Task
     Ok(number_to_be_squared * number_to_be_squared)
 }
 
+pub async fn square_async(
+    _task_index: u32,
+    number_to_be_squared: U256,
+) -> Result<U256, TaskManagerError> {
+    Ok(number_to_be_squared * number_to_be_squared)
+}
+
 // Implement the [`TaskManagerDefs`] trait for a unit struct.
 // You need to specify the input and output types of the task. In this case, U256.
 // You also need to specify the selectors for the new task event and the task responded event.
