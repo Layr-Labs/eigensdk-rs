2f91055b53b4fdde72421d0c9e4463ec63a4abaa
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-13 16:54:29 -0300

    refactor: use AsyncFn instead of Future

--- crates/challenger/src/challenger_processor.rs
+++ crates/challenger/src/challenger_processor.rs
@@ -7,30 +7,26 @@ use eigen_task_processor::task_manager::{TaskManager, TaskManagerError};
 use eigen_task_processor::task_response_metadata_sol::TaskResponseMetadataSol;
 use eigen_task_processor::{task::Task, task_response::TaskResponse};
 use eigen_utils::slashing::middleware::iblssignaturechecker::BN254::G1Point;
-use futures_util::future::BoxFuture;
 use std::collections::HashMap;
-use std::future::Future;
 use tracing::{error, info};
 
 #[derive(Debug)]
-pub struct IndexingChallengerProcessor<TM, F, Fut>
+pub struct IndexingChallengerProcessor<TM, F>
 where
     TM: TaskManager + Send + Sync + 'static + Clone,
-    F: Fn(Task<TM::Input>, TaskResponse<TM::Output>) -> Fut + Send + Sync,
-    Fut: Future<Output = Result<bool, TaskManagerError>>,
+    F: AsyncFn(Task<TM::Input>, TaskResponse<TM::Output>) -> Result<bool, TaskManagerError>,
 {
     task_manager: TM,
     tasks: HashMap<u32, Task<TM::Input>>,
     is_response_correct: F,
 }
 
-impl<TM, F, Fut> ChallengerTaskProcessor for IndexingChallengerProcessor<TM, F, Fut>
+impl<TM, F> ChallengerTaskProcessor for IndexingChallengerProcessor<TM, F>
 where
     TM: TaskManager + Send + Sync + 'static + Clone,
     TM::Input: From<<<TM::Input as SolValue>::SolType as SolType>::RustType>,
     TM::Output: From<<<TM::Output as SolValue>::SolType as SolType>::RustType>,
-    F: Fn(Task<TM::Input>, TaskResponse<TM::Output>) -> Fut + Send + Sync,
-    Fut: Future<Output = Result<bool, TaskManagerError>> + Send,
+    F: AsyncFn(Task<TM::Input>, TaskResponse<TM::Output>) -> Result<bool, TaskManagerError>,
 {
     type Input = TM::Input;
 
@@ -89,13 +85,12 @@ where
     }
 }
 
-impl<TM, F, Fut> IndexingChallengerProcessor<TM, F, Fut>
+impl<TM, F> IndexingChallengerProcessor<TM, F>
 where
     TM: TaskManager + Send + Sync + 'static + Clone,
     TM::Input: From<<<TM::Input as SolValue>::SolType as SolType>::RustType>,
     TM::Output: From<<<TM::Output as SolValue>::SolType as SolType>::RustType>,
-    F: Fn(Task<TM::Input>, TaskResponse<TM::Output>) -> Fut + Send + Sync,
-    Fut: Future<Output = Result<bool, TaskManagerError>>,
+    F: AsyncFn(Task<TM::Input>, TaskResponse<TM::Output>) -> Result<bool, TaskManagerError>,
 {
     pub fn new(task_manager: TM, is_response_correct: F) -> Self {
         Self {
@@ -106,23 +101,16 @@ where
     }
 }
 
-type VerifyFuture = BoxFuture<'static, Result<bool, TaskManagerError>>;
-
-pub fn verifier_from_compute_function<Input, Output, CrFn, CrFut>(
-    compute_response: CrFn,
-) -> impl Fn(Task<Input>, TaskResponse<Output>) -> VerifyFuture + Clone + Send + Sync
+pub fn verifier_from_compute_function<Input, Output, F>(
+    compute_response: F,
+) -> impl AsyncFn(Task<Input>, TaskResponse<Output>) -> Result<bool, TaskManagerError>
 where
-    CrFn: Fn(u32, Input) -> CrFut + Clone + Send + Sync + 'static,
-    CrFut: Future<Output = Result<Output, TaskManagerError>> + Send + 'static,
-    Output: SolValue + Clone + PartialEq + Send + 'static,
-    Input: Send + 'static,
+    F: AsyncFn(u32, Input) -> Result<Output, TaskManagerError> + Clone,
+    Output: SolValue + Clone + PartialEq,
 {
-    move |task, task_response| {
+    async move |task: Task<Input>, task_response: TaskResponse<Output>| {
         let fut = compute_response(task_response.task_index, task.input);
-
-        Box::pin(async move {
-            let computed = fut.await?;
-            Ok(computed == task_response.response)
-        })
+        let computed = fut.await?;
+        Ok(computed == task_response.response)
     }
 }
