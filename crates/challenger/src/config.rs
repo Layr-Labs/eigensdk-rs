8dca5f4a8c5e2d62b19ed7881fb944c4c5f850e6
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-14 17:29:30 -0300

    refactor: rename fields

--- crates/challenger/src/config.rs
+++ crates/challenger/src/config.rs
@@ -3,8 +3,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 /// Configuration for the [`Challenger`]
 pub struct ChallengerConfig {
-    /// The rpc url
-    pub rpc_url: String,
-    /// The websocket url
-    pub ws_url: String,
+    /// HTTP RPC URL
+    pub http_rpc_url: String,
+    /// WebSocket RPC URL
+    pub ws_rpc_url: String,
 }
