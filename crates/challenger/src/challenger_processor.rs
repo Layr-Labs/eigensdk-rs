5400bc12817f05f40e6cb271d6502f2c0c8b4f82
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-14 11:27:01 -0300

    revert: unintended changes

--- crates/challenger/src/challenger_processor.rs
+++ crates/challenger/src/challenger_processor.rs
@@ -70,7 +70,6 @@ where
         // If the response is incorrect, we need to raise a challenge
         let tm = self.task_manager.clone();
         let task = task.clone();
-        info!("Raising challenge for task {task_index}");
         tokio::spawn(async move {
             tm.raise_challenge(
                 task,
