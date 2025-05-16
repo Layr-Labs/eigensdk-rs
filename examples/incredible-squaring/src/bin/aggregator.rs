e37d57ffd1d30fb225bc27492df080bd57f53d9e
ricomateo <mrico@fi.uba.ar>
2025-05-16 16:08:38 -0300

    fix addresses and init logger in aggregator

--- examples/incredible-squaring/src/bin/aggregator.rs
+++ examples/incredible-squaring/src/bin/aggregator.rs
@@ -8,6 +8,8 @@ use alloy::transports::http::reqwest::Url;
 use eigen_aggregator::task_processor::IndexingTaskProcessor;
 use eigen_aggregator::{Aggregator, AggregatorConfig};
 use eigen_logging::get_logger;
+use eigen_logging::init_logger;
+use eigen_logging::log_level::LogLevel;
 use incredible_squaring::bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::IncredibleSquaringTaskManagerInstance;
 use incredible_squaring::utils::load_config;
 use std::str::FromStr;
@@ -15,11 +17,12 @@ use std::time::Duration;
 
 #[tokio::main]
 async fn main() {
+    init_logger(LogLevel::Info);
     let config: AggregatorConfig = load_config("./src/config/squaring-aggregator.toml").unwrap();
     let logger = get_logger();
-    let signer = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    let signer = "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6";
     let task_manager_address =
-        Address::from_str("0x742d35cc6634c0532925a3b844f51254ab06f58e").unwrap();
+        Address::from_str("0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3").unwrap();
     let url = Url::parse(&config.http_rpc_url).unwrap();
     let wallet = EthereumWallet::new(PrivateKeySigner::from_str(signer).unwrap());
     let provider = ProviderBuilder::new().wallet(wallet).on_http(url);
