08f611db6dccf13773752529c4978fa16a87bde5
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-19 12:48:08 -0300

    docs: add docs to challenger structs and traits

--- crates/challenger/src/challenger.rs
+++ crates/challenger/src/challenger.rs
@@ -8,6 +8,7 @@ use eigen_utils::slashing::middleware::iblssignaturechecker::BN254::G1Point;
 
 use crate::error::ChallengerError;
 
+/// Challenger Task Processor trait that defines the logic for handling new tasks and task responses
 pub trait ChallengerTaskProcessor {
     /// Input type of the task
     type Input: Clone + SolValue + Send + Sync + 'static;
