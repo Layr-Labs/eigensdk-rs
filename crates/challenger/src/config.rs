9087ebd4e4b430b5879427705b932dc2e16f2615
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-20 09:40:28 -0300

    docs: fix linking

--- crates/challenger/src/config.rs
+++ crates/challenger/src/config.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-/// Configuration for the `Challenger`
+/// Configuration for the [`Challenger`](crate::Challenger)
 pub struct ChallengerConfig {
     /// HTTP RPC URL
     pub http_rpc_url: String,
