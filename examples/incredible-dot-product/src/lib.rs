043210cc5a8c718753b703f72c0153fc5d81cff4
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-23 17:49:52 -0300

    docs(v2): add missing documentation to examples lib (#531)
    ### What Changed?
    <!-- Describe the changes made in this pull request -->
    
    ### Reviewer Checklist
    
    - [ ] New features are tested and documented
    - [ ] PR updates the changelog with a description of changes
    - [ ] PR has one of the `changelog-X` labels (if applies)
    - [ ] Code deprecates any old functionality before removing it
    
    ---------
    
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- examples/incredible-dot-product/src/lib.rs
+++ examples/incredible-dot-product/src/lib.rs
@@ -1,6 +1,17 @@
-//! Incredible Dot Product Example
+//! AVS Example - Incredible Dot Product
 //!
-//! This example demonstrates how to use the SDK to build an AVS.
+//! This example proposes a more complex AVS than the incredible squaring.
+//! The input type handled is a struct that contains two vectors, representing two points,
+//! that should be processed making the dot product, being the output type a U256,
+//! representing the result of the product. In this sense, the task for the operators
+//! to complete is executing the dot product between the two points, and returning the
+//! result as the response value submitted to the Task Manager on-chain contract.
+//!
+//! To learn how to implement each module, refer to the corresponding binary files.
+//! Each file contains step-by-step instructions for setting up the module.
+//!
+//! For more details about the example logic and how to run it, see the
+//! [README](https://github.com/Layr-Labs/eigensdk-rs/blob/v2-dev-2/examples/incredible-dot-product/README.md).
 
 /// Bindings for the Incredible Dot Product Operator
 pub mod bindings;
