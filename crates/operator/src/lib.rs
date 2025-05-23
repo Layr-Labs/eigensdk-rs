45fc6d62f8741d3bcc281f3c736c931fef75a845
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-23 14:19:24 -0300

    Update crates/operator/src/lib.rs
    Co-authored-by: Maximo Palopoli <96491141+maximopalopoli@users.noreply.github.com>

--- crates/operator/src/lib.rs
+++ crates/operator/src/lib.rs
@@ -140,8 +140,8 @@ pub mod register_config;
 /// Operator registration utils
 pub mod registration;
 
-/// The operator listen for [`NEW_TASK_EVENT_SELECTOR`](eigen_task_manager::TaskManagerDefs::NEW_TASK_EVENT_SELECTOR)
-/// events and compute the task response and sign it with the operator's BLS key pair.
+/// The operator listens for [`NEW_TASK_EVENT_SELECTOR`](eigen_task_manager::TaskManagerDefs::NEW_TASK_EVENT_SELECTOR)
+/// events, computes the task response, and signs it with the operator's BLS key pair.
 /// Then, it sends the signed task response to the aggregator via RPC.
 ///
 /// To more in-depth details about the operator, refer to the [module documentation](https://github.com/Layr-Labs/eigensdk-rs/blob/v2-dev-2/crates/operator/src/lib.rs#L1-L110).
