fc23b77c1aea3df0fad01b7ff6645605760d90e9
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-06-05 19:37:52 -0300

    test: copy test from signer

--- crates/signer/src/signer_v2/mod.rs
+++ crates/signer/src/signer_v2/mod.rs
@@ -82,3 +82,220 @@ pub async fn tx_signer_from_config(
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::signer_v2::tx_signer_from_config;
+
+    use super::SignerConfig;
+    use alloy::consensus::{SignableTransaction, TxLegacy};
+    use alloy::network::{TxSigner, TxSignerSync};
+    use alloy::primitives::PrimitiveSignature;
+    use alloy::primitives::{address, bytes, hex_literal::hex, keccak256, Address, U256};
+    use alloy::signers::local::PrivateKeySigner;
+    use aws_config::{BehaviorVersion, Region, SdkConfig};
+    use aws_sdk_kms::{
+        self,
+        config::{Credentials, SharedCredentialsProvider},
+        types::KeyMetadata,
+    };
+    use eigen_testing_utils::anvil::start_anvil_container;
+    use eigen_testing_utils::test_data::TestData;
+    use std::str::FromStr;
+    use testcontainers::{
+        core::{IntoContainerPort, WaitFor},
+        runners::AsyncRunner,
+        ContainerAsync, GenericImage, ImageExt,
+    };
+    use tokio;
+
+    const PRIVATE_KEY: &str = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7";
+    const ADDRESS: [u8; 20] = hex!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045");
+    const SIGNATURE_R: &str =
+        "99963972037857174861280476053118856715670512199525969754644366601434507134123";
+    const SIGNATURE_S: &str =
+        "54587766196536123534774489028213321677166972433316011091332824361042811624091";
+    const KEYSTORE_PATH: &str = "mockdata/dummy.key.json";
+    const KEYSTORE_PASSWORD: &str = "testpassword";
+    const LOCALSTACK_PORT: u16 = 4566;
+    const AWS_US_WEST_REGION: &str = "us-west-1";
+    const LOCALSTACK_IMAGE_NAME: &str = "localstack/localstack";
+    const LOCALSTACK_IMAGE_TAG: &str = "latest";
+
+    #[tokio::test]
+    async fn sign_transaction_with_private_key() {
+        let config = SignerConfig::PrivateKey {
+            private_key_hex: PRIVATE_KEY.into(),
+        };
+        let mut tx = TxLegacy {
+            to: Address::from(ADDRESS).into(),
+            value: U256::from(1_000_000_000),
+            gas_limit: 2_000_000,
+            nonce: 0,
+            gas_price: 21_000_000_000,
+            input: bytes!(),
+            chain_id: Some(1),
+        };
+
+        let signer = tx_signer_from_config(config).await.unwrap();
+
+        let signature: [u8; 65] = signer.sign_transaction(&mut tx).await.unwrap().into();
+        let sig = PrimitiveSignature::try_from(&signature[..]).unwrap();
+        let expected_signature = PrimitiveSignature::new(
+            U256::from_str(SIGNATURE_R).unwrap(),
+            U256::from_str(SIGNATURE_S).unwrap(),
+            false,
+        );
+        assert_eq!(sig, expected_signature);
+    }
+
+    #[tokio::test]
+    async fn sign_transaction_with_keystore() {
+        let config = SignerConfig::Keystore {
+            path: KEYSTORE_PATH.into(),
+            password: KEYSTORE_PASSWORD.into(),
+        };
+        let mut tx = TxLegacy {
+            to: Address::from(ADDRESS).into(),
+            value: U256::from(1_000_000_000),
+            gas_limit: 2_000_000,
+            nonce: 0,
+            gas_price: 21_000_000_000,
+            input: bytes!(),
+            chain_id: Some(1),
+        };
+
+        let private_key = hex!("7a28b5ba57c53603b0b07b56bba752f7784bf506fa95edc395f5cf6c7514fe9d");
+        let expected_signer = PrivateKeySigner::from_slice(&private_key).unwrap();
+        let expected_signature = expected_signer.sign_transaction_sync(&mut tx).unwrap();
+
+        let signer = tx_signer_from_config(config).await.unwrap();
+        let signature = signer.sign_transaction(&mut tx).await.unwrap();
+
+        assert_eq!(signature, expected_signature);
+    }
+
+    #[tokio::test]
+    async fn test_sign_transaction_with_kms_signer() {
+        // Start the container running Localstack
+        let _container = start_localstack_container().await;
+
+        let localstack_endpoint = format!("http://localhost:{}", LOCALSTACK_PORT);
+        let config = get_aws_config(
+            "localstack".into(),
+            "localstack".into(),
+            Region::from_static(AWS_US_WEST_REGION),
+            localstack_endpoint.clone(),
+        )
+        .await;
+
+        let default_tx = TxLegacy {
+            to: address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into(),
+            value: U256::from(1_000_000_000),
+            gas_limit: 2_000_000,
+            nonce: 0,
+            gas_price: 21_000_000_000,
+            input: bytes!(),
+            chain_id: Some(1),
+        };
+        let mut test_data: TestData<TxLegacy> = TestData::new(default_tx);
+
+        // Create an AWS KMS Client
+        let client = aws_sdk_kms::Client::new(&config);
+
+        // Create a key
+        let key_metadata = create_kms_key(&client).await;
+
+        // Create a signer for the given key
+        let key_id = key_metadata.key_id();
+        let chain_id = Some(1);
+        let signer = tx_signer_from_config(SignerConfig::Aws {
+            key_id: key_id.into(),
+            chain_id,
+            access_key: "localstack".into(),
+            secret_access_key: "localstack".into(),
+            region: AWS_US_WEST_REGION.into(),
+            endpoint_url: localstack_endpoint,
+        })
+        .await
+        .unwrap();
+
+        // Sign the transaction
+        let signature = signer.sign_transaction(&mut test_data.input).await.unwrap();
+
+        // Recover the address
+        let mut encoded_tx = Vec::new();
+        test_data.input.encode_for_signing(&mut encoded_tx);
+        let prehash = keccak256(encoded_tx);
+        let recovered_address = signature.recover_address_from_prehash(&prehash).unwrap();
+
+        // Check that the recovered addresses are the same
+        assert_eq!(signer.address(), recovered_address);
+    }
+
+    #[tokio::test]
+    async fn test_sign_legacy_transaction_with_web3_signer() {
+        let (_container, endpoint, _ws_endpoint) = start_anvil_container().await;
+
+        let address = address!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+        let signer = tx_signer_from_config(SignerConfig::Web3 { endpoint, address })
+            .await
+            .unwrap();
+        let mut tx = TxLegacy {
+            to: address!("a0Ee7A142d267C1f36714E4a8F75612F20a79720").into(),
+            value: U256::from(1_000_000_000),
+            gas_limit: 0x76c0,
+            gas_price: 21_000_000_000,
+            nonce: 0,
+            input: bytes!(),
+            chain_id: Some(31337),
+        };
+
+        let signature = signer.sign_transaction(&mut tx).await.unwrap();
+
+        let private_key_hex = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+        let expected_signer = PrivateKeySigner::from_str(private_key_hex).unwrap();
+        let expected_signature = expected_signer.sign_transaction_sync(&mut tx).unwrap();
+
+        assert_eq!(signature, expected_signature);
+    }
+
+    async fn start_localstack_container() -> ContainerAsync<GenericImage> {
+        GenericImage::new(LOCALSTACK_IMAGE_NAME, LOCALSTACK_IMAGE_TAG)
+            .with_exposed_port(LOCALSTACK_PORT.tcp())
+            .with_wait_for(WaitFor::message_on_stdout("Ready."))
+            .with_mapped_port(LOCALSTACK_PORT, LOCALSTACK_PORT.tcp())
+            .start()
+            .await
+            .expect("Error starting localstack container")
+    }
+
+    async fn create_kms_key(client: &aws_sdk_kms::Client) -> KeyMetadata {
+        client
+            .create_key()
+            .key_spec(aws_sdk_kms::types::KeySpec::EccSecgP256K1)
+            .key_usage(aws_sdk_kms::types::KeyUsageType::SignVerify)
+            .send()
+            .await
+            .unwrap()
+            .key_metadata()
+            .unwrap()
+            .clone()
+    }
+
+    async fn get_aws_config(
+        access_key: String,
+        secret_access_key: String,
+        region: Region,
+        endpoint_url: String,
+    ) -> SdkConfig {
+        let creds = Credentials::new(access_key, secret_access_key, None, None, "Static");
+        aws_config::load_defaults(BehaviorVersion::latest())
+            .await
+            .to_builder()
+            .credentials_provider(SharedCredentialsProvider::new(creds))
+            .endpoint_url(endpoint_url)
+            .region(Some(region.clone()))
+            .build()
+    }
+}
