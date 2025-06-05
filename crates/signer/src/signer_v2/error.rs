34ecc07b8fd6249cfa0652eedf3ca591bfa752be
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-06-05 18:08:06 -0300

    feat: add error file for signer v2

---
+++ crates/signer/src/signer_v2/error.rs
@@ -0,0 +1,13 @@
+use alloy::signers::{aws::AwsSignerError, local::LocalSignerError};
+use thiserror::Error;
+
+/// Possible errors raised in signer creation
+#[derive(Error, Debug)]
+pub enum SignerError {
+    /// LocalSigner error
+    #[error("local signer error: {0}")]
+    LocalSignerError(#[from] LocalSignerError),
+    /// AwsSigner error
+    #[error("aws signer error: {0}")]
+    AwsSignerError(#[from] AwsSignerError),
+}
