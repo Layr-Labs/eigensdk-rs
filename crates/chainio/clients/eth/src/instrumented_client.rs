b122a8898c6c4df0949a4c6c6d8b493d4eba11d3
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-06-11 16:43:43 -0300

    refactor: use connect_http and connect_ws in client-eth crate

--- crates/chainio/clients/eth/src/instrumented_client.rs
+++ crates/chainio/clients/eth/src/instrumented_client.rs
@@ -111,7 +111,7 @@ impl InstrumentedClient {
         let url = Url::parse(url).map_err(|_| InstrumentedClientError::InvalidUrl)?;
         let http_client = ProviderBuilder::new()
             .disable_recommended_fillers()
-            .on_http(url);
+            .connect_http(url);
         let net_version = http_client
             .get_net_version()
             .await
@@ -145,7 +145,7 @@ impl InstrumentedClient {
 
         let ws_client = ProviderBuilder::new()
             .disable_recommended_fillers()
-            .on_ws(ws_connect)
+            .connect_ws(ws_connect)
             .await
             .unwrap();
         let net_version = ws_client
