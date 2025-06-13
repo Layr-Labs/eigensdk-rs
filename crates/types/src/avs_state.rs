ed589e22a695e07c28934050cf95fec4eed71bcf
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-06-13 16:14:47 -0300

    feat: create operatorSet struct

--- crates/types/src/avs_state.rs
+++ crates/types/src/avs_state.rs
@@ -1,9 +1,12 @@
 use crate::operator::OperatorId;
 use crate::operator::OperatorInfo;
 use crate::operator::QuorumNum;
+use alloy::primitives::Address;
 use alloy::primitives::U256;
 use eigen_crypto_bls::BlsG1Point;
 use ethers::types::U64;
+use serde::Deserialize;
+use serde::Serialize;
 use std::collections::HashMap;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -20,4 +23,13 @@ pub struct QuorumAvsState {
     pub total_stake: U256,
     pub agg_pub_key_g1: BlsG1Point,
     pub block_num: u64,
+}
+
+///An operator set identified by the AVS address and an identifier
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct OperatorSet {
+    /// The unique identifier for the operator set
+    pub id: u32,
+    /// The address of the AVS this operator set belongs to
+    pub avs: Address,
 }
