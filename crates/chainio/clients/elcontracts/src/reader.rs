3cfb1b218e1b3cabaf1e8475c354613f6d6b8151
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-06-24 17:14:21 -0300

    test: fix wrong address in test_get_strategy_and_underlying_token

--- crates/chainio/clients/elcontracts/src/reader.rs
+++ crates/chainio/clients/elcontracts/src/reader.rs
@@ -1654,13 +1654,13 @@ mod tests {
         let underlying_token_addr_str = underlying_token_addr.to_string();
         assert_eq!(
             underlying_token_addr_str,
-            "0x36C02dA8a0983159322a80FFE9F24b1acfF8B570"
+            "0x4c5859f0f772848b2d91f1d83e2fe57935348029"
         );
 
         let strategy_contract_addr_str = strategy_contract_addr.address().to_string();
         assert_eq!(
             strategy_contract_addr_str,
-            "0xeC4cFde48EAdca2bC63E94BB437BbeAcE1371bF3"
+            "0xec4cfde48eadca2bc63e94bb437bbeace1371bf3"
         );
     }
 
