d1590068b1d04ec388059334a05d67d3d61d6749
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-06-24 17:32:19 -0300

    test: fix address

--- crates/chainio/clients/elcontracts/src/reader.rs
+++ crates/chainio/clients/elcontracts/src/reader.rs
@@ -1654,7 +1654,7 @@ mod tests {
         let underlying_token_addr_str = underlying_token_addr.to_string();
         assert_eq!(
             underlying_token_addr_str,
-            "0x4c5859f0f772848b2d91f1d83e2fe57935348029"
+            "0x4c5859f0F772848b2D91F1D83E2Fe57935348029"
         );
 
         let strategy_contract_addr_str = strategy_contract_addr.address().to_string();
